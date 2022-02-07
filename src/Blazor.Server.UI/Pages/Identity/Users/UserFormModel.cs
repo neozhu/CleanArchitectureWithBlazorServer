2d71a4ffc85e4e785ac396f1b68b6ba4a5f9fa4d
neozhu <neo.js.cn@gmail.com>
2022-02-07 17:37:48 +0800

    wip: create user form

---
+++ src/Blazor.Server.UI/Pages/Identity/Users/UserFormModel.cs
@@ -0,0 +1,14 @@
+namespace Blazor.Server.UI.Pages.Identity.Users;
+
+public class UserFormModel
+{
+    public string? Id { get; set; }
+    public string? UserName { get; set; }
+    public string? DisplayName { get; set; }
+    public string? Site { get; set; }
+    public string? ProfilePictureDataUrl { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? ConfirmPassword { get; set; }
+    public string[]? AssignRoles { get; set; }
+}
