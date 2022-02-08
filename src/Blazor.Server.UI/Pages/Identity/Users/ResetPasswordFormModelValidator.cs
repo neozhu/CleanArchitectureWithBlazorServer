46de418ee2346e04854c4431cb262146518b4268
neozhu <new163@163.com>
2022-02-08 16:15:03 +0800

    WIP: change password form

---
+++ src/Blazor.Server.UI/Pages/Identity/Users/ResetPasswordFormModelValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+namespace Blazor.Server.UI.Pages.Identity.Users;
+
+public class ResetPasswordFormModelValidator : AbstractValidator<ResetPasswordFormModel>
+{
+    public ResetPasswordFormModelValidator()
+    {
+        RuleFor(p => p.Password).NotEmpty().WithMessage("Your password cannot be empty")
+                 .MinimumLength(6).WithMessage("Your password length must be at least 6.")
+                 .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
+                 .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+                 .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+                 .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
+                 .Matches(@"[\!\?\*\.]+").WithMessage("Your password must contain at least one (!? *.).");
+        RuleFor(x => x.ConfirmPassword)
+             .Equal(x => x.Password);
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<ResetPasswordFormModel>.CreateWithOptions((ResetPasswordFormModel)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
