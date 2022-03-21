c641fb88ad67d728a7d3b29c09c6e31351d734a2
neozhu <neo.js.cn@gmail.com>
2022-03-21 14:29:10 +0800

    Update ValidationBehaviour.cs

--- src/Application/Common/Behaviours/ValidationBehaviour.cs
+++ src/Application/Common/Behaviours/ValidationBehaviour.cs
@@ -19,19 +19,8 @@ where TRequest : IRequest<TResponse>
     {
         if (_validators.Any())
         {
-            var context = new ValidationContext<TRequest>(request);
-
-            var validationResults = await Task.WhenAll(
-                _validators.Select(v =>
-                    v.ValidateAsync(context, cancellationToken)));
-
-            var failures = validationResults
-                .Where(r => r.Errors.Any())
-                .SelectMany(r => r.Errors)
-                .ToList();
-
-            if (failures.Any())
-                throw new ValidationException(failures);
+            foreach (var validator in _validators)
+                validator.ValidateAndThrow(request);
         }
         return await next();
     }
