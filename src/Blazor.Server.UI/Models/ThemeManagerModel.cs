ebfabd96477cdc6bbc7087956791375f9206713b
neozhu <neo.js.cn@gmail.com>
2022-03-22 17:30:39 +0800

    update resources

--- src/Blazor.Server.UI/Models/ThemeManagerModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Blazor.Server.UI.Models;
-
-public class ThemeManagerModel
-{
-    public bool IsDarkMode { get; set; } = false;
-    public string PrimaryColor { get; set; } = "#2d4275";
-    public double BorderRadius { get; set; } = 4;
-}
-
