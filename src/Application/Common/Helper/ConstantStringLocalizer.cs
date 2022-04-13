3c1f1096902196110579888e7c592418d4123ad7
neozhu <neo.js.cn@gmail.com>
2022-04-13 19:29:36 +0800

    Update ConstantStringLocalizer.cs

--- src/Application/Common/Helper/ConstantStringLocalizer.cs
+++ src/Application/Common/Helper/ConstantStringLocalizer.cs
@@ -4,9 +4,10 @@ using System.Resources;
 namespace CleanArchitecture.Blazor.Application.Common.Helper;
 public static class ConstantStringLocalizer
 {
+    public const string RESOURCEID = "CleanArchitecture.Blazor.Application.Common.Resources.ConstantString";
     private static readonly ResourceManager rm;
     static ConstantStringLocalizer() {
-        rm = new ResourceManager("CleanArchitecture.Blazor.Application.Resources.Constants.Constants", typeof(ConstantStringLocalizer).Assembly);
+        rm = new ResourceManager(RESOURCEID, typeof(ConstantStringLocalizer).Assembly);
     }
     public static string Localize(string key)
     {
