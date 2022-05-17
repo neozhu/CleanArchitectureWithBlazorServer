f58a3c2c4ca80f9d2b65d9a83681757b5e0c337e
neozhu <neo.js.cn@gmail.com>
2022-05-17 17:06:02 +0800

    Update MappingProfile.cs

--- src/Application/Common/Mappings/MappingProfile.cs
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -14,19 +14,40 @@ public class MappingProfile : Profile
 
     private void ApplyMappingsFromAssembly(Assembly assembly)
     {
-        var types = assembly.GetExportedTypes()
-        .Where(t => t.GetInterfaces().Any(i =>
-            i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
-        .ToList();
+        var mapFromType = typeof(IMapFrom<>);
+
+        var mappingMethodName = nameof(IMapFrom<object>.Mapping);
+
+        bool HasInterface(Type t) => t.IsGenericType && t.GetGenericTypeDefinition() == mapFromType;
+
+        var types = assembly.GetExportedTypes().Where(t => t.GetInterfaces().Any(HasInterface)).ToList();
+
+        var argumentTypes = new Type[] { typeof(Profile) };
 
         foreach (var type in types)
         {
             var instance = Activator.CreateInstance(type);
 
-            var methodInfo = type.GetMethod("Mapping")
-                ?? type.GetInterface("IMapFrom`1")!.GetMethod("Mapping");
+            var methodInfo = type.GetMethod(mappingMethodName);
 
-            methodInfo?.Invoke(instance, new object[] { this });
+            if (methodInfo != null)
+            {
+                methodInfo.Invoke(instance, new object[] { this });
+            }
+            else
+            {
+                var interfaces = type.GetInterfaces().Where(HasInterface).ToList();
+
+                if (interfaces.Count > 0)
+                {
+                    foreach (var @interface in interfaces)
+                    {
+                        var interfaceMethodInfo = @interface.GetMethod(mappingMethodName, argumentTypes);
+
+                        interfaceMethodInfo?.Invoke(instance, new object[] { this });
+                    }
+                }
+            }
 
         }
     }
