bc576542a764ff2dca28ddf31b00451974c6e870
neozhu <neo.js.cn@gmail.com>
2022-05-23 19:06:57 +0800

    add blazorui service configuration

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -12,7 +12,7 @@ namespace CleanArchitecture.Blazor.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -9,7 +9,7 @@ namespace CleanArchitecture.Blazor.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<AuditableEntitySaveChangesInterceptor>();
         if (configuration.GetValue<bool>("UseInMemoryDatabase"))
