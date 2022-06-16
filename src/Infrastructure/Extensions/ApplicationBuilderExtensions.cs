d3622be5f26d57f1d44a1a4e4338059ec67cf565
neozhu <neo.js.cn@gmail.com>
2022-06-16 20:07:40 +0800

    update nuget

--- src/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ src/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -4,8 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.FileProviders;
-using Serilog;
-using Serilog.Context;
+
 
 namespace CleanArchitecture.Blazor.Infrastructure.Extensions;
 
@@ -13,21 +12,6 @@ public static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app, IConfiguration config)
     {
-        app.UseSerilogRequestLogging(options =>
-        {
-            options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
-            {
-                //This didn't work when tested
-                diagnosticContext.Set("UserName", httpContext.User?.Identity?.Name ?? "Anonymous");
-            };
-        });
-        app.Use(async (httpContext, next) =>
-        {
-            //This is the correct implementation 
-            var userName = httpContext.User?.Identity?.Name ?? "Anonymous"; //Gets user Name from user Identity
-            LogContext.PushProperty("UserName", userName); //Push user in LogContext;
-            await next.Invoke();
-        });
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
