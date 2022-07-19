a35acd363223f103cb7ec2d6950e1e6ad9ecfbbc
neozhu <new163@163.com>
2022-07-19 13:59:55 +0800

    refactoring code

--- src/Blazor.Server.UI/Models/SideMenu/MenuSectionItemModel.cs
+++ src/Blazor.Server.UI/Models/SideMenu/MenuSectionItemModel.cs
@@ -2,7 +2,7 @@ namespace Blazor.Server.UI.Models.SideMenu;
 
 public class MenuSectionItemModel
 {
-    public string? Title { get; set; }
+    public string Title { get; set; }=string.Empty;
     public string? Icon { get; set; }
     public string? Href { get; set; }
     public string[]? Roles { get; set; }
--- src/Blazor.Server.UI/Models/SideMenu/MenuSectionSubItemModel.cs
+++ src/Blazor.Server.UI/Models/SideMenu/MenuSectionSubItemModel.cs
@@ -2,7 +2,7 @@ namespace Blazor.Server.UI.Models.SideMenu;
 
 public class MenuSectionSubItemModel
 {
-    public string? Title { get; set; }
+    public string Title { get; set; }=string.Empty;
     public string? Href { get; set; }
     public string[]? Roles { get; set; }
     public PageStatus PageStatus { get; set; } = PageStatus.Completed;
