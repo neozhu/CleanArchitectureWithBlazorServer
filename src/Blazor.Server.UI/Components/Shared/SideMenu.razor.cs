5c038447361269dc9192834f57ca0985ad5f359c
Diyar Bakir <DBHRuner@Gmail.com>
2022-07-26 18:37:20 +0300

    Improve RTL Layout

--- src/Blazor.Server.UI/Components/Shared/SideMenu.razor.cs
+++ src/Blazor.Server.UI/Components/Shared/SideMenu.razor.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using Blazor.Server.UI.Models.SideMenu;
+using Blazor.Server.UI.Services;
 using Blazor.Server.UI.Services.Navigation;
 using CleanArchitecture.Blazor.Application.Common.Models;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -12,17 +13,29 @@ public partial class SideMenu:IDisposable
 {
     private IEnumerable<MenuSectionModel> _menuSections = new List<MenuSectionModel>();
 
-    [EditorRequired] [Parameter] public bool SideMenuDrawerOpen { get; set; } 
-    [EditorRequired] [Parameter] public EventCallback<bool> SideMenuDrawerOpenChanged { get; set; }
-    [EditorRequired] [Parameter] public UserProfile UserProfile { get; set; } = default!;
+    [EditorRequired] [Parameter] 
+    public bool SideMenuDrawerOpen { get; set; } 
+    
+    [EditorRequired] [Parameter]
+    public EventCallback<bool> SideMenuDrawerOpenChanged { get; set; }
+   
+    [EditorRequired] [Parameter] 
+    public UserProfile UserProfile { get; set; } = default!;
 
-    [Inject] private IMenuService _menuService { get; set; } = default!;
+    [Inject] 
+    private IMenuService _menuService { get; set; } = default!;
+    
     [CascadingParameter]
     protected Task<AuthenticationState> _authState { get; set; } = default!;
+    
     [Inject]
     private AuthenticationStateProvider _authenticationStateProvider { get; set; } = default!;
-
+    
+    [Inject] 
+    private LayoutService LayoutService { get; set; } = default!;
+    
     private string[] _roles = new string[] { };
+    
     protected override async Task OnInitializedAsync()
     {
         var authstate = await _authState;
