8ff768e878874aeff0c86b98671c8c11b85d8db4
neozhu <neo.js.cn@gmail.com>
2022-08-09 19:22:11 +0800

    fixed unit test

--- tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -1,7 +1,11 @@
+using CleanArchitecture.Blazor.Application.Common.Behaviours;
 using CleanArchitecture.Blazor.Application.Common.Interfaces;
 using CleanArchitecture.Blazor.Application.Common.Interfaces.Identity;
+using CleanArchitecture.Blazor.Application.Features.Products.Commands.AddEdit;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CleanArchitecture.Blazor.Application.UnitTests.Common.Behaviours;
@@ -10,26 +14,31 @@ public class RequestLoggerTests
 {
     private readonly Mock<ICurrentUserService> _currentUserService;
     private readonly Mock<IIdentityService> _identityService;
-
+    private readonly Mock<ILogger<AddEditProductCommand>> _logger;
 
     public RequestLoggerTests()
     {
         _currentUserService = new Mock<ICurrentUserService>();
         _identityService = new Mock<IIdentityService>();
+        _logger = new Mock<ILogger<AddEditProductCommand>>();
     }
 
     [Test]
-    public  Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
+    public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
     {
         _currentUserService.Setup(x => x.UserId()).Returns(Task.FromResult("Administrator"));
-        _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
-       return Task.CompletedTask;
+        var requestLogger = new LoggingBehaviour<AddEditProductCommand>(_logger.Object, _currentUserService.Object);
+        await requestLogger.Process(new AddEditProductCommand {  Brand= "Brand", Name= "Brand", Price=1.0m, Unit="EA" }, new CancellationToken());
+        _currentUserService.Verify(i => i.UserName(), Times.Once);
+     
     }
 
     [Test]
-    public  Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
+    public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
     {
-        _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
-        return Task.CompletedTask;
+        var requestLogger = new LoggingBehaviour<AddEditProductCommand>(_logger.Object, _currentUserService.Object);
+        await requestLogger.Process(new AddEditProductCommand { Brand = "Brand", Name = "Brand", Price = 1.0m, Unit = "EA" }, new CancellationToken());
+        _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Never);
+ 
     }
 }
