e4bbea107d9f007725d37a5de1b2e5276a9cf82a
neozhu <new163@163.com>
2022-08-25 11:35:12 +0800

    add chart component for logview

--- src/Application/Features/Loggers/DTOs/LogTimeLineDto.cs
+++ src/Application/Features/Loggers/DTOs/LogTimeLineDto.cs
@@ -3,15 +3,10 @@
 
 namespace CleanArchitecture.Blazor.Application.Features.Logs.DTOs;
 
-public class LogLevelChartDto
-{
-    public string level { get; set; } = "Information";
-    public int total { get; set; }
-}
 
 public class LogTimeLineDto
 {
-    public DateTime time { get; set; }
+    public DateTime dt { get; set; }
     public int total { get; set; }
-    public string level { get; set; } = "Information";
+
 }
