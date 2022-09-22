cc12ce4487ee98254c64b2f755c2b2a69af80048
neozhu <new163@163.com>
2022-09-22 17:08:06 +0800

    fixed signalr bug

--- src/Infrastructure/Hubs/SignalRHub.cs
+++ src/Infrastructure/Hubs/SignalRHub.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Concurrent;
 using CleanArchitecture.Blazor.Infrastructure.Constants;
+using DocumentFormat.OpenXml.Office2010.Excel;
 using Microsoft.AspNetCore.SignalR;
 
 
@@ -28,22 +29,39 @@ public class SignalRHub : Hub<ISignalRHub>
     }
     public override async Task OnConnectedAsync()
     {
-
+        var id = Context.ConnectionId;
+        if (!_onlineUsers.ContainsKey(id))
+        {
+            _onlineUsers.TryAdd(id, String.Empty);
+        }
         await base.OnConnectedAsync();
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        string id = Context.ConnectionId;
+        var id = Context.ConnectionId;
         //try to remove key from dictionary
         if (_onlineUsers.TryRemove(id, out string? userId))
         {
-          await Clients.All.Disconnect(userId);
+          await Disconnect(userId);
         }
         await base.OnDisconnectedAsync(exception);
     }
 
-    public async Task OnConnectUser(string userId)
+
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendMessage(message);
+    }
+    public async Task SendNotification(string message)
+    {
+        await Clients.All.SendNotification(message);
+    }
+    public async Task Completed(string message)
+    {
+        await Clients.All.Completed(message);
+    }
+    public async Task Connect(string userId)
     {
         var id = Context.ConnectionId;
         if (!_onlineUsers.ContainsKey(id))
@@ -55,17 +73,15 @@ public class SignalRHub : Hub<ISignalRHub>
                 await Clients.All.Connect(userId);
             }
         }
+        else
+        {
+            _onlineUsers.TryUpdate(id, userId, userId);
+            await Clients.All.Connect(userId);
+        }
+      
     }
-    public async Task SendMessage(string userId, string message)
+    public async Task Disconnect(string userId)
     {
-        await Clients.All.SendMessage(message);
-    }
-    public async Task SendNotification(string message)
-    {
-        await Clients.All.SendNotification(message);
-    }
-    public async Task Completed(string message)
-    {
-        await Clients.All.Completed(message);
+        await Clients.All.Disconnect(userId);
     }
 }
