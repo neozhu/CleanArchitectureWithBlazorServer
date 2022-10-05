cb3a7d464aa5427257661456bec22d04f5e0e9fd
neozhu <neo.js.cn@gmail.com>
2022-10-05 11:08:21 +0800

    update resource and nuget packages

--- src/Application/Common/Behaviours/CacheInvalidationBehaviour.cs
+++ src/Application/Common/Behaviours/CacheInvalidationBehaviour.cs
@@ -19,7 +19,7 @@ public class CacheInvalidationBehaviour<TRequest, TResponse> : IPipelineBehavior
         _cache = cache;
         _logger = logger;
     }
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request,  RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         _logger.LogTrace("{Name} cache expire with {@Request}.", nameof(request), request);
         var response = await next();
--- src/Application/Common/Behaviours/CachingBehaviour.cs
+++ src/Application/Common/Behaviours/CachingBehaviour.cs
@@ -19,7 +19,7 @@ public class CachingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest,
         _cache = cache;
         _logger = logger;
     }
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         _logger.LogTrace("{Name} is caching with {@Request}.", nameof(request),request);
         var response = await _cache.GetOrAddAsync(
