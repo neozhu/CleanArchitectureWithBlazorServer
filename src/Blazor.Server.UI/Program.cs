726399e9985f379dd13face75d1c087a1b122199
neozhu <new163@163.com>
2022-10-19 17:21:59 +0800

    ApplicationDbContextInitialiser replace of applicationdbcontextseed

--- src/Blazor.Server.UI/Program.cs
+++ src/Blazor.Server.UI/Program.cs
@@ -29,54 +29,31 @@ builder.Services.AddInfrastructureServices(builder.Configuration)
 
 var app = builder.Build();
 
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    // Initialise and seed database
+    using (var scope = app.Services.CreateScope())
+    {
+
+        var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+        await initialiser.InitialiseAsync();
+        await initialiser.SeedAsync();
+        var notificationService = scope.ServiceProvider.GetService<INotificationService>();
+        if (notificationService is InMemoryNotificationService inmemoryService)
+        {
+            inmemoryService.Preload();
+        }
+    }
+}
+else { 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseExceptionHandler("/Error");
 app.UseInfrastructure(builder.Configuration);
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    try
-    {
-        var context = services.GetRequiredService<ApplicationDbContext>();
-
-        if (context.Database.IsSqlServer())
-        {
-            context.Database.Migrate();
-        }
-
-        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<ApplicationRole>>();
-
-        await ApplicationDbContextSeed.SeedSampleDataAsync(context);
-        await ApplicationDbContextSeed.SeedDefaultUserAsync(context, userManager, roleManager);
-
-    }
-    catch (Exception ex)
-    {
-        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-
-        logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-
-        throw;
-    }
-
-    var notificationService = scope.ServiceProvider.GetService<INotificationService>();
-    if (notificationService is InMemoryNotificationService inmemoryService)
-    {
-        inmemoryService.Preload();
-    }
-}
-
 
 await app.RunAsync();
---
+++ src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,147 @@
+﻿using System.Reflection;
+using CleanArchitecture.Blazor.Infrastructure.Constants.Role;
+
+namespace CleanArchitecture.Blazor.Infrastructure.Persistence;
+public class ApplicationDbContextInitialiser
+{
+    private readonly ILogger<ApplicationDbContextInitialiser> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<ApplicationRole> _roleManager;
+
+    public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+    {
+        _logger = logger;
+        _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            if (_context.Database.IsSqlServer())
+            {
+                await _context.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+    private static IEnumerable<string> GetAllPermissions()
+    {
+        var allPermissions = new List<string>();
+        var modules = typeof(Permissions).GetNestedTypes();
+
+        foreach (var module in modules)
+        {
+            var moduleName = string.Empty;
+            var moduleDescription = string.Empty;
+
+            var fields = module.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+            foreach (var fi in fields)
+            {
+                var propertyValue = fi.GetValue(null);
+
+                if (propertyValue is not null)
+                    allPermissions.Add((string)propertyValue);
+            }
+        }
+
+        return allPermissions;
+    }
+    public async Task TrySeedAsync()
+    {
+        // Default tenants
+        if (!_context.Tenants.Any())
+        {
+            _context.Tenants.Add(new Tenant() { Name = "Master", Description = "Master Site" });
+            _context.Tenants.Add(new Tenant() { Name = "Slave", Description = "Slave Site" });
+            await _context.SaveChangesAsync();
+
+        }
+
+        // Default roles
+        var administratorRole = new ApplicationRole(RoleConstants.AdministratorRole) { Description = "Admin Group" };
+        var userRole = new ApplicationRole(RoleConstants.BasicRole) { Description = "Basic Group" };
+        var Permissions = GetAllPermissions();
+        if (_roleManager.Roles.All(r => r.Name != administratorRole.Name))
+        {
+            await _roleManager.CreateAsync(administratorRole);
+           
+            foreach (var permission in Permissions)
+            {
+                await _roleManager.AddClaimAsync(administratorRole, new System.Security.Claims.Claim(ApplicationClaimTypes.Permission, permission));
+            }
+        }
+        if (_roleManager.Roles.All(r => r.Name != userRole.Name))
+        {
+            await _roleManager.CreateAsync(userRole);
+            foreach (var permission in Permissions)
+            {
+                if (permission.StartsWith("Permissions.Products"))
+                    await _roleManager.AddClaimAsync(userRole, new System.Security.Claims.Claim(ApplicationClaimTypes.Permission, permission));
+            }
+        }
+        // Default users
+        var administrator = new ApplicationUser { UserName = "Administrator", Provider = "Local", IsActive = true, TenantId = _context.Tenants.First().Id, TenantName = _context.Tenants.First().Name, DisplayName = "Administrator", Email = "new163@163.com", EmailConfirmed = true, ProfilePictureDataUrl = $"https://s.gravatar.com/avatar/78be68221020124c23c665ac54e07074?s=80" };
+        var demo = new ApplicationUser { UserName = "Demo", IsActive = true, Provider = "Local", TenantId = _context.Tenants.First().Id, TenantName = _context.Tenants.First().Name, DisplayName = "Demo", Email = "neozhu@126.com", EmailConfirmed = true, ProfilePictureDataUrl = $"https://s.gravatar.com/avatar/ea753b0b0f357a41491408307ade445e?s=80" };
+
+
+        if (_userManager.Users.All(u => u.UserName != administrator.UserName))
+        {
+            await _userManager.CreateAsync(administrator, RoleConstants.DefaultPassword);
+            await _userManager.AddToRolesAsync(administrator, new[] { administratorRole.Name });
+        }
+        if (_userManager.Users.All(u => u.UserName != demo.UserName))
+        {
+            await _userManager.CreateAsync(demo, RoleConstants.DefaultPassword);
+            await _userManager.AddToRolesAsync(demo, new[] { userRole.Name });
+        }
+
+        // Default data
+        // Seed, if necessary
+        if (!_context.KeyValues.Any())
+        {
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Status", Value = "initialization", Text = "initialization", Description = "Status of workflow" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Status", Value = "processing", Text = "processing", Description = "Status of workflow" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Status", Value = "pending", Text = "pending", Description = "Status of workflow" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Status", Value = "finished", Text = "finished", Description = "Status of workflow" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Brand", Value = "Apple", Text = "Apple", Description = "Brand of production" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Brand", Value = "MI", Text = "MI", Description = "Brand of production" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Brand", Value = "Logitech", Text = "Logitech", Description = "Brand of production" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Brand", Value = "Linksys", Text = "Linksys", Description = "Brand of production" });
+
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Unit", Value = "EA", Text = "EA", Description = "Unit of product" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Unit", Value = "KM", Text = "KM", Description = "Unit of product" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Unit", Value = "PC", Text = "PC", Description = "Unit of product" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Unit", Value = "KG", Text = "KG", Description = "Unit of product" });
+            _context.KeyValues.Add(new Domain.Entities.KeyValue() { Name = "Unit", Value = "ST", Text = "ST", Description = "Unit of product" });
+            await _context.SaveChangesAsync();
+
+        }
+        if (!_context.Products.Any())
+        {
+            _context.Products.Add(new Domain.Entities.Product() { Brand = "Apple", Name = "IPhone 13 Pro", Description = "Apple iPhone 13 Pro smartphone. Announced Sep 2021. Features 6.1″ display, Apple A15 Bionic chipset, 3095 mAh battery, 1024 GB storage.", Unit = "EA", Price = 999.98m });
+            _context.Products.Add(new Domain.Entities.Product() { Brand = "MI", Name = "MI 12 Pro", Description = "Xiaomi 12 Pro Android smartphone. Announced Dec 2021. Features 6.73″ display, Snapdragon 8 Gen 1 chipset, 4600 mAh battery, 256 GB storage.", Unit = "EA", Price = 199.00m });
+            _context.Products.Add(new Domain.Entities.Product() { Brand = "Logitech", Name = "MX KEYS Mini", Description = "Logitech MX Keys Mini Introducing MX Keys Mini – a smaller, smarter, and mightier keyboard made for creators. Type with confidence on a keyboard crafted for efficiency, stability, and...", Unit = "PA", Price = 99.90m });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
