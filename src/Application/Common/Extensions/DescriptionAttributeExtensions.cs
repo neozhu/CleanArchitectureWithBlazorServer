dc96e7a5b9d3dd79cf63f27c12d465c30b7ebf10
neozhu <new163@163.com>
2022-10-28 09:35:35 +0800

    remove  ToDescriptionString()

--- src/Application/Common/Extensions/DescriptionAttributeExtensions.cs
+++ src/Application/Common/Extensions/DescriptionAttributeExtensions.cs
@@ -3,39 +3,35 @@
 namespace CleanArchitecture.Blazor.Application.Common.Extensions;
 public static class DescriptionAttributeExtensions
 {
-    public static string ToDescriptionString(this Enum val)
-    {
-        var attributes = (DescriptionAttribute[]?)val.GetType().GetField(val.ToString())?.GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-        return attributes?.Length > 0
-            ? attributes[0].Description
-            : val.ToString();
-    }
-    public static string GetEnumDescriptionOrName(this Enum e)
+    public static string GetDescription(this Enum e)
     {
         var name = e.ToString();
         var memberInfo = e.GetType().GetMember(name)[0];
         var descriptionAttributes = memberInfo.GetCustomAttributes(typeof(DescriptionAttribute), inherit: false);
-        if (!descriptionAttributes.Any())
-            return name;
-        return ((DescriptionAttribute)descriptionAttributes.First()).Description;
+        if (descriptionAttributes.Any())
+        {
+            return ((DescriptionAttribute)descriptionAttributes.First()).Description;
+        }
+        return name;
     }
     public static string GetMemberDescription<T>(this T t, string memberName) where T : class
     {
         var memberInfo = t.GetType().GetMember(memberName)[0];
-        var descriptionAttributes = memberInfo.GetCustomAttributes(typeof(DescriptionAttribute), inherit: false);
+     var descriptionAttributes = memberInfo.GetCustomAttributes(typeof(DescriptionAttribute), inherit: false);
         if (descriptionAttributes.Any())
+        {
             return (descriptionAttributes.First() as DescriptionAttribute)!.Description;
-        else
-            return memberName;
+        }
+        return memberName;
     }
     public static string GetClassDescription<T>(this T t) where T : class
     {
         var descriptionAttributes = t.GetType().GetCustomAttributes(typeof(DescriptionAttribute), inherit: false);
         if (descriptionAttributes.Any())
+        {
             return (descriptionAttributes.First() as DescriptionAttribute)!.Description;
-        else
-            return nameof(t);
+        }
+        return nameof(t);
 
     }
 }
