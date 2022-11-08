34e33b9c9aff2aa8806876e4f106ac39aad970af
neozhu <new163@163.com>
2022-11-08 10:48:20 +0800

    Use IMediator instead of ISender

--- src/Application/Features/Customers/Commands/AddEdit/AddEditCustomerCommandValidator.cs
+++ src/Application/Features/Customers/Commands/AddEdit/AddEditCustomerCommandValidator.cs
@@ -7,11 +7,11 @@ public class AddEditCustomerCommandValidator : AbstractValidator<AddEditCustomer
 {
     public AddEditCustomerCommandValidator()
     {
-        // TODO: Implement AddEditCustomerCommandValidator method, for example: 
-        RuleFor(v => v.Name)
-             .MaximumLength(256)
-             .NotEmpty();
-        //throw new System.NotImplementedException();
+           // TODO: Implement AddEditCustomerCommandValidator method, for example: 
+           // RuleFor(v => v.Name)
+           //      .MaximumLength(256)
+           //      .NotEmpty();
+           throw new System.NotImplementedException();
      }
      public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
      {
--- src/Application/Features/Customers/DTOs/CustomerDto.cs
+++ src/Application/Features/Customers/DTOs/CustomerDto.cs
@@ -5,15 +5,19 @@ using System.ComponentModel;
 namespace CleanArchitecture.Blazor.Application.Features.Customers.DTOs;
 
 
-public class CustomerDto : IMapFrom<Customer>
+public class CustomerDto:IMapFrom<Customer>
 {
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Customer, CustomerDto>().ReverseMap();
+    }
     // TODO: define data transfer object (DTO) fields, for example:
-    [Description("Id")]
-    public int Id { get; set; }
+        [Description("Id")]
+    public int Id {get;set;} 
     [Description("Name")]
-    public string? Name { get; set; }
+    public string Name {get;set;} = String.Empty; 
     [Description("Description")]
-    public string? Description { get; set; }
+    public string? Description {get;set;} 
 
 }
 
