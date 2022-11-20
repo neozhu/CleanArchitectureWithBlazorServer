55fd1db10502947a41e0335eff159b571a61f282
neozhu <neo.js.cn@gmail.com>
2022-11-20 21:11:52 +0800

    change user menu card

--- src/Application/Common/Models/UserProfile.cs
+++ src/Application/Common/Models/UserProfile.cs
@@ -7,8 +7,8 @@ public class UserProfile
     public string? SuperiorId { get; set; }
     public string? ProfilePictureDataUrl { get; set; }
     public string? DisplayName { get; set; }
-    public string? UserName { get; set; }
-    public string? Email { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string? PhoneNumber { get; set; }
     public string? Role { get; set; }
     public string[]? AssignRoles { get; set; }
