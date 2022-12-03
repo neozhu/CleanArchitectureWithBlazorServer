aef9450c3abdb002bb9bcb9da6e61b884bd653cc
neozhu <neo.js.cn@gmail.com>
2022-12-03 12:07:12 +0800

    add themeprofiles

---
+++ src/Application/Features/ThemeProfiles/Actions/UpdateThemeProfilesAction.cs
@@ -0,0 +1,15 @@
+﻿using BlazorState;
+
+namespace CleanArchitecture.Blazor.Application.Features.ThemeProfiles;
+public partial class ThemeProfilesState
+{
+    public class UpdateThemeProfilesAction : IAction
+    {
+        public UpdateThemeProfilesAction(UserPreferences userPreferences)
+        {
+            Preferences = userPreferences;
+        }
+        public UserPreferences Preferences { get; private set; }
+    }
+     
+}
