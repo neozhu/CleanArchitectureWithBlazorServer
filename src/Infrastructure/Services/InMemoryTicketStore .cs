6acf72ffa827213d89c0367eb7602c0d4213a9af
neozhu <neo.js.cn@gmail.com>
2022-12-04 18:33:15 +0800

    Update InMemoryTicketStore .cs

--- src/Infrastructure/Services/InMemoryTicketStore .cs
+++ src/Infrastructure/Services/InMemoryTicketStore .cs
@@ -3,13 +3,19 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace CleanArchitecture.Blazor.Infrastructure.Services;
+/// <summary>
+/// Adapted from https://github.com/aspnet/Security/blob/dev/samples/CookieSessionSample/MemoryCacheTicketStore.cs
+/// to manage large identity cookies.
+/// More info: http://www.dotnettips.info/post/2581
+/// And http://www.dotnettips.info/post/2575
+/// </summary>
 public class InMemoryTicketStore : ITicketStore
 {
     private readonly IMemoryCache _cache;
 
     public InMemoryTicketStore(IMemoryCache cache)
     {
-        _cache = cache;
+        _cache = cache ?? throw new ArgumentNullException(nameof(_cache)); ;
     }
 
     public Task RemoveAsync(string key)
@@ -21,25 +27,35 @@ public class InMemoryTicketStore : ITicketStore
 
     public Task<AuthenticationTicket> RetrieveAsync(string key)
     {
-        var ticket = _cache.Get<AuthenticationTicket>(key);
-
+        _cache.TryGetValue(key, out AuthenticationTicket ticket);
         return Task.FromResult(ticket);
     }
 
     public Task RenewAsync(string key, AuthenticationTicket ticket)
     {
-        _cache.Set(key, ticket);
+        var options = new MemoryCacheEntryOptions().SetSize(1);
+        var expiresUtc = ticket.Properties.ExpiresUtc;
+        if (expiresUtc.HasValue)
+        {
+            options.SetAbsoluteExpiration(expiresUtc.Value);
+        }
 
-        return Task.CompletedTask;
+        if (ticket.Properties.AllowRefresh ?? false)
+        {
+            options.SetSlidingExpiration(TimeSpan.FromMinutes(60));//TODO: configurable.
+        }
+
+        _cache.Set(key, ticket, options);
+
+        return Task.FromResult(0);
     }
 
-    public Task<string> StoreAsync(AuthenticationTicket ticket)
+    public async Task<string> StoreAsync(AuthenticationTicket ticket)
     {
         var key = ticket.Principal.Claims
           .First(c => c.Type == ClaimTypes.Name).Value;
 
-        _cache.Set(key, ticket);
-
-        return Task.FromResult(key);
+        await RenewAsync(key, ticket);
+        return key;
     }
 }
