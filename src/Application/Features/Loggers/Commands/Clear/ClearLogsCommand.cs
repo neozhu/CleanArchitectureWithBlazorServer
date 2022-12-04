e7227ac63955a19143b51b963530f7b900dbee22
neozhu <neo.js.cn@gmail.com>
2022-12-04 12:16:54 +0800

    add clear log command

---
+++ src/Application/Features/Loggers/Commands/Clear/ClearLogsCommand.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+
+
+using CleanArchitecture.Blazor.Application.Common.Interfaces;
+using CleanArchitecture.Blazor.Application.Features.Loggers.Caching;
+
+namespace CleanArchitecture.Blazor.Application.Features.Loggers.Commands.Delete;
+
+public class ClearLogsCommand : IRequest<Result>, ICacheInvalidator
+{
+
+    public string CacheKey => LogsCacheKey.GetAllCacheKey;
+    public CancellationTokenSource? SharedExpiryTokenSource => LogsCacheKey.SharedExpiryTokenSource();
+
+}
+
+public class ClearLogsCommandHandler :
+             IRequestHandler<ClearLogsCommand, Result>
+
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly IStringLocalizer<ClearLogsCommandHandler> _localizer;
+    public ClearLogsCommandHandler(
+        IApplicationDbContext context,
+        IStringLocalizer<ClearLogsCommandHandler> localizer,
+         IMapper mapper
+        )
+    {
+        _context = context;
+        _localizer = localizer;
+        _mapper = mapper;
+    }
+    public async Task<Result> Handle(ClearLogsCommand request, CancellationToken cancellationToken)
+    {
+        _context.Loggers.RemoveRange(_context.Loggers);
+        await _context.SaveChangesAsync(cancellationToken);
+        return await Result.SuccessAsync();
+    }
+
+}
+
