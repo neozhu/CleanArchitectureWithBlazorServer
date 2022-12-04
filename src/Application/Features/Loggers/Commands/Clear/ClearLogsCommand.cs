8f13e302adfb7c6d158df4e0a91f7e9f79788859
neozhu <neo.js.cn@gmail.com>
2022-12-04 12:29:51 +0800

    change logs

--- src/Application/Features/Loggers/Commands/Clear/ClearLogsCommand.cs
+++ src/Application/Features/Loggers/Commands/Clear/ClearLogsCommand.cs
@@ -5,6 +5,7 @@
 
 using CleanArchitecture.Blazor.Application.Common.Interfaces;
 using CleanArchitecture.Blazor.Application.Features.Loggers.Caching;
+using static CleanArchitecture.Blazor.Application.Constants.Permissions;
 
 namespace CleanArchitecture.Blazor.Application.Features.Loggers.Commands.Delete;
 
@@ -23,20 +24,25 @@ public class ClearLogsCommandHandler :
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
     private readonly IStringLocalizer<ClearLogsCommandHandler> _localizer;
+    private readonly ILogger<ClearLogsCommandHandler> _logger;
+
     public ClearLogsCommandHandler(
         IApplicationDbContext context,
         IStringLocalizer<ClearLogsCommandHandler> localizer,
+        ILogger<ClearLogsCommandHandler> logger,
          IMapper mapper
         )
     {
         _context = context;
         _localizer = localizer;
+        _logger = logger;
         _mapper = mapper;
     }
     public async Task<Result> Handle(ClearLogsCommand request, CancellationToken cancellationToken)
     {
         _context.Loggers.RemoveRange(_context.Loggers);
         await _context.SaveChangesAsync(cancellationToken);
+        _logger.LogInformation("Logs are erased");
         return await Result.SuccessAsync();
     }
 
