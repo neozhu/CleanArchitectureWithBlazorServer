ddce0cbcde1ae3e4d65f658f8b18e805a73a99ba
neozhu <neo.js.cn@gmail.com>
2022-12-04 12:16:54 +0800

    add clear log command

--- src/Application/Constants/Permission/Permissions.cs
+++ src/Application/Constants/Permission/Permissions.cs
@@ -23,6 +23,7 @@ public static class Permissions
         public const string View = "Permissions.Logs.View";
         public const string Search = "Permissions.Logs.Search";
         public const string Export = "Permissions.Logs.Export";
+        public const string Purge = "Permissions.Logs.Purge";
     }
     
 
