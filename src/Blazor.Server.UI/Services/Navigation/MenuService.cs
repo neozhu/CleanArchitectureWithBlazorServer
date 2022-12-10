b340cef48d013d9a0e802c4c201f6e16af298f85
neozhu <neo.js.cn@gmail.com>
2022-12-10 20:33:47 +0800

    refactoring code

--- src/Blazor.Server.UI/Services/Navigation/MenuService.cs
+++ src/Blazor.Server.UI/Services/Navigation/MenuService.cs
@@ -42,7 +42,7 @@ public class MenuService : IMenuService
                 new()
                 {
                     Title = "Analytics",
-                    Roles=new string[]{ RoleConstants.AdministratorRole, RoleConstants.UsersRole },
+                    Roles=new string[]{ RoleNameConstants.AdministratorRole, RoleNameConstants.Users },
                     Icon = Icons.Material.Filled.Analytics,
                     Href = "/analytics",
                     PageStatus = PageStatus.ComingSoon
@@ -50,7 +50,7 @@ public class MenuService : IMenuService
                 new()
                 {
                     Title = "Banking",
-                    Roles=new string[]{ RoleConstants.AdministratorRole,RoleConstants.UsersRole },
+                    Roles=new string[]{ RoleNameConstants.AdministratorRole,RoleNameConstants.Users },
                     Icon = Icons.Material.Filled.Money,
                     Href = "/banking",
                     PageStatus = PageStatus.ComingSoon
@@ -58,7 +58,7 @@ public class MenuService : IMenuService
                 new()
                 {
                     Title = "Booking",
-                    Roles=new string[]{ RoleConstants.AdministratorRole,RoleConstants.UsersRole },
+                    Roles=new string[]{ RoleNameConstants.AdministratorRole,RoleNameConstants.Users },
                     Icon = Icons.Material.Filled.CalendarToday,
                     Href = "/booking",
                     PageStatus = PageStatus.ComingSoon
@@ -69,7 +69,7 @@ public class MenuService : IMenuService
         new MenuSectionModel
         {
             Title = "MANAGEMENT",
-            Roles=new string[]{ RoleConstants.AdministratorRole },
+            Roles=new string[]{ RoleNameConstants.AdministratorRole },
             SectionItems = new List<MenuSectionItemModel>
             {
                 new()
