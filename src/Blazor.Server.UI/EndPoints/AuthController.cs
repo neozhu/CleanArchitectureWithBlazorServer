5124550287983527849bb4ac843fc81120914972
neozhu <new163@163.com>
2022-12-15 13:42:31 +0800

    refactoring blazor component

--- src/Blazor.Server.UI/EndPoints/AuthController.cs
+++ src/Blazor.Server.UI/EndPoints/AuthController.cs
@@ -106,8 +106,8 @@ public class AuthController : Controller
     public async Task<IActionResult> Logout()
     {
         var userId = _signInManager.Context.User.GetUserId();
-        var identityUser = await _userManager.FindByIdAsync(userId);
-        identityUser.IsLive = true;
+        var identityUser = await _userManager.FindByIdAsync(userId) ?? throw new NotFoundException($"Application user not found.");
+        identityUser.IsLive = false;
         await _userManager.UpdateAsync(identityUser);
         _logger.LogInformation("{@UserName} logout successful.", identityUser.UserName);
         await _signInManager.SignOutAsync();
