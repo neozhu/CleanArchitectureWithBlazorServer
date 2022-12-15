842905839db0ea79615c64a939f60fa4cd36a623
neozhu <new163@163.com>
2022-12-15 13:15:05 +0800

    refactoring identityservices

--- src/Application/Common/Interfaces/Identity/IIdentityService.cs
+++ src/Application/Common/Interfaces/Identity/IIdentityService.cs
@@ -13,7 +13,6 @@ public interface IIdentityService : IService
     Task<string?> GetUserNameAsync(string userId, CancellationToken cancellation = default);
     Task<bool> IsInRoleAsync(string userId, string role, CancellationToken cancellation = default);
     Task<bool> AuthorizeAsync(string userId, string policyName, CancellationToken cancellation = default);
-    Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password, CancellationToken cancellation = default);
     Task<Result> DeleteUserAsync(string userId, CancellationToken cancellation = default);
     Task<IDictionary<string, string?>> FetchUsers(string roleName, CancellationToken cancellation = default);
     Task UpdateLiveStatus(string userId, bool isLive, CancellationToken cancellation = default);
