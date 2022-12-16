9fd6fccfbf2a5ae0d8a941f69231df77df232432
neozhu <new163@163.com>
2022-12-16 10:44:10 +0800

    add javascript interop

---
+++ src/Blazor.Server.UI/Services/JsInterop/OpenSeadragon.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace Blazor.Server.UI.Services.JsInterop;
+
+[SupportedOSPlatform("browser")]
+public partial class OpenSeadragon
+{
+    [JSImport("", "")]
+    internal static partial Task Show(string element, string url);
+}
