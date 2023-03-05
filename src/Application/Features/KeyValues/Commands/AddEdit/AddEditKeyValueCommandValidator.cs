7b4c8753f71abd464ddbd36ff2c3b0e1c827ec1c
hualin.zhu <neo.js.cn@gmail.com>
2023-03-05 09:57:23 +0800

    update picklist page

--- src/Application/Features/KeyValues/Commands/AddEdit/AddEditKeyValueCommandValidator.cs
+++ src/Application/Features/KeyValues/Commands/AddEdit/AddEditKeyValueCommandValidator.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using CleanArchitecture.Blazor.Application.Features.Tenants.Commands.AddEdit;
+
 namespace CleanArchitecture.Blazor.Application.Features.KeyValues.Commands.AddEdit;
 
 public class AddEditKeyValueCommandValidator : AbstractValidator<AddEditKeyValueCommand>
@@ -8,12 +10,15 @@ public class AddEditKeyValueCommandValidator : AbstractValidator<AddEditKeyValue
     public AddEditKeyValueCommandValidator()
     {
         RuleFor(v => v.Name).NotNull();
-        RuleFor(v => v.Text)
-            .MaximumLength(256)
-            .NotEmpty();
-        RuleFor(v => v.Value)
-            .MaximumLength(256)
-            .NotEmpty();
+        RuleFor(v => v.Text).MaximumLength(256).NotEmpty();
+        RuleFor(v => v.Value).MaximumLength(256).NotEmpty();
 
     }
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<AddEditKeyValueCommand>.CreateWithOptions((AddEditKeyValueCommand)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
 }
