b2aa17a4d3b66897cb034a78ea6ea937e96f17cd
hualin.zhu <neo.js.cn@gmail.com>
2023-04-05 15:00:57 +0800

    remove FluxorComponent

--- src/Application/Common/Interfaces/ICurrentUserService.cs
+++ src/Application/Common/Interfaces/ICurrentUserService.cs
@@ -7,5 +7,7 @@ public interface ICurrentUserService
 {
     string? UserId { get; set; }
     string? UserName { get; set; }
+    string? TenantId { get; set; }
+    string? TenantName { get; set;}
  
 }
