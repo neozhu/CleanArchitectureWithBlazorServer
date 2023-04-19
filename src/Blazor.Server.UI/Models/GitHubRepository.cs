af780a1781f36a4c04a322048341211891ef211f
hualin.zhu <neo.js.cn@gmail.com>
2023-04-20 07:59:27 +0800

    update

---
+++ src/Blazor.Server.UI/Models/GitHubRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazor.Server.UI.Models;
+
+public class GitHubRepository
+{
+    [JsonPropertyName("stargazers_count")]
+    public int StargazersCount { get; set; }
+}
