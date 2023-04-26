21274a885752501254b359735b68a29be17b33a4
Bram1903 <70259613+Bram1903@users.noreply.github.com>
2023-04-26 10:32:43 +0200

    Added: Npgsql.EnableLegacyTimestampBehavior when using PostgreSQL

--- src/Infrastructure/Extensions/DbContextOptionsBuilderExtensions.cs
+++ src/Infrastructure/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CleanArchitecture.Blazor.Infrastructure.Constants.Database;
+﻿using CleanArchitecture.Blazor.Infrastructure.Constants.Database;
 
 namespace CleanArchitecture.Blazor.Infrastructure.Extensions;
+
 internal static class DbContextOptionsBuilderExtensions
 {
-    internal static DbContextOptionsBuilder UseDatabase(this DbContextOptionsBuilder builder, string dbProvider, string connectionString)
+    internal static DbContextOptionsBuilder UseDatabase(this DbContextOptionsBuilder builder, string dbProvider,
+        string connectionString)
     {
-        return dbProvider.ToLowerInvariant() switch
+        switch (dbProvider.ToLowerInvariant())
         {
-            DbProviderKeys.Npgsql => builder.UseNpgsql(connectionString, e =>
-                                 e.MigrationsAssembly("CleanArchitecture.Blazor.Migrators.PostgreSQL")),
-            DbProviderKeys.SqlServer => builder.UseSqlServer(connectionString, e =>
-                                 e.MigrationsAssembly("CleanArchitecture.Blazor.Migrators.MSSQL")),
-            DbProviderKeys.SqLite => builder.UseSqlite(connectionString, e =>
-                                 e.MigrationsAssembly("CleanArchitecture.Blazor.Migrators.SqLite")),
-            _ => throw new InvalidOperationException($"DB Provider {dbProvider} is not supported."),
-        };
+            case DbProviderKeys.Npgsql:
+                AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+                return builder.UseNpgsql(connectionString,
+                    e => e.MigrationsAssembly("CleanArchitecture.Blazor.Migrators.PostgreSQL"));
+            case DbProviderKeys.SqlServer:
+                return builder.UseSqlServer(connectionString,
+                    e => e.MigrationsAssembly("CleanArchitecture.Blazor.Migrators.MSSQL"));
+            case DbProviderKeys.SqLite:
+                return builder.UseSqlite(connectionString,
+                    e => e.MigrationsAssembly("CleanArchitecture.Blazor.Migrators.SqLite"));
+            default:
+                throw new InvalidOperationException($"DB Provider {dbProvider} is not supported.");
+        }
     }
 }
