c8e5fa1e29da60e6b9e04f582c61aa87097e047d
Bram1903 <70259613+Bram1903@users.noreply.github.com>
2023-05-07 17:07:12 +0200

    Small refactoring

--- src/Infrastructure/Hubs/HubClient.cs
+++ src/Infrastructure/Hubs/HubClient.cs
@@ -4,52 +4,68 @@ using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace CleanArchitecture.Blazor.Infrastructure.Hubs;
+
 public class HubClient : IAsyncDisposable
 {
+    public delegate Task MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+
     private readonly HubConnection _hubConnection;
-    private bool _started = false;
+    private bool _started;
+
     public HubClient(NavigationManager navigationManager,
-        AccessTokenProvider  authProvider
-)
+        AccessTokenProvider authProvider
+    )
     {
-        var token = authProvider.AccessToken;
+        string? token = authProvider.AccessToken;
         string hubUrl = navigationManager.BaseUri.TrimEnd('/') + SignalR.HubUrl;
         _hubConnection = new HubConnectionBuilder()
-              .WithUrl(hubUrl, options => {
-                  options.AccessTokenProvider =()=> Task.FromResult(token);
-                  options.Transports = HttpTransportType.WebSockets;
-                  })
-              .Build();
+            .WithUrl(hubUrl, options =>
+            {
+                options.AccessTokenProvider = () => Task.FromResult(token);
+                options.Transports = HttpTransportType.WebSockets;
+            })
+            .Build();
         _hubConnection.ServerTimeout = TimeSpan.FromSeconds(30);
-        _hubConnection.On<string>(SignalR.OnConnect, (userId) =>
+        _hubConnection.On<string>(SignalR.OnConnect, userId =>
         {
             Login?.Invoke(this, userId);
         });
 
-        _hubConnection.On<string>(SignalR.OnDisconnect, (userId) =>
+        _hubConnection.On<string>(SignalR.OnDisconnect, userId =>
         {
             Logout?.Invoke(this, userId);
         });
-        _hubConnection.On<string>(SignalR.SendNotification, (message) =>
+        _hubConnection.On<string>(SignalR.SendNotification, message =>
         {
             NotificationReceived?.Invoke(this, message);
         });
-        _hubConnection.On<string, string>(SignalR.SendMessage, (from,message) =>
-        {
-            HandleReceiveMessage(from,message);
-        });
-        _hubConnection.On<string>(SignalR.JobCompleted, (message) =>
+        _hubConnection.On<string, string>(SignalR.SendMessage, HandleReceiveMessage);
+        _hubConnection.On<string>(SignalR.JobCompleted, message =>
         {
             JobCompleted?.Invoke(this, message);
         });
-        _hubConnection.On<string>(SignalR.JobStart, (message) =>
+        _hubConnection.On<string>(SignalR.JobStart, message =>
         {
             JobStarted?.Invoke(this, message);
         });
     }
+
+    public async ValueTask DisposeAsync()
+    {
+        try { await _hubConnection.StopAsync(); }
+        finally
+        {
+            await _hubConnection.DisposeAsync();
+        }
+    }
+
     public async Task StartAsync(CancellationToken cancellation = default)
     {
-        if (_started) return;
+        if (_started)
+        {
+            return;
+        }
+
         _started = true;
         await _hubConnection.StartAsync(cancellation);
     }
@@ -60,30 +76,23 @@ public class HubClient : IAsyncDisposable
         // raise an event to subscribers
         MessageReceived?.Invoke(this, new MessageReceivedEventArgs(from, message));
     }
-    
+
     public async Task SendAsync(string message)
     {
         await _hubConnection.SendAsync(SignalR.SendMessage, message);
     }
+
     public async Task NotifyAsync(string message)
     {
         await _hubConnection.SendAsync(SignalR.SendNotification, message);
     }
-    public async ValueTask DisposeAsync()
-    {
-        try { await _hubConnection.StopAsync(); }
-        finally
-        {
-            await _hubConnection.DisposeAsync();
-        }
-    }
+
     public event EventHandler<string>? Login;
     public event EventHandler<string>? JobStarted;
     public event EventHandler<string>? JobCompleted;
     public event EventHandler<string>? Logout;
     public event EventHandler<string>? NotificationReceived;
     public event MessageReceivedEventHandler? MessageReceived;
-    public delegate Task MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
 
     public class MessageReceivedEventArgs : EventArgs
     {
@@ -92,6 +101,7 @@ public class HubClient : IAsyncDisposable
             UserId = userId;
             Message = message;
         }
+
         public string UserId { get; set; }
         public string Message { get; set; }
     }
