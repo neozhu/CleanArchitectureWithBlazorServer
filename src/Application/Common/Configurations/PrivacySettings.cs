130ba51b2457f23959f39f2b308be01560ebb51d
Bram1903 <70259613+Bram1903@users.noreply.github.com>
2023-05-12 15:42:16 +0200

    Improved the configuration documentation

--- src/Application/Common/Configurations/PrivacySettings.cs
+++ src/Application/Common/Configurations/PrivacySettings.cs
@@ -1,9 +1,22 @@
 namespace CleanArchitecture.Blazor.Application.Common.Configurations;
 
+/// <summary>
+///     Configuration wrapper for the privacy section
+/// </summary>
 public class PrivacySettings
 {
-    public const string Privacy = "Privacy";
+    /// <summary>
+    ///     Privacy key constraint
+    /// </summary>
+    public const string Key = nameof(PrivacySettings);
     
+    /// <summary>
+    ///     When enabled, the logs will include client IP addresses
+    /// </summary>
     public bool LogClientIpAddresses { get; set; } = true;
+    
+    /// <summary>
+    ///     When enabled, the logs will include the client agents
+    /// </summary>
     public bool LogClientAgents { get; set; } = true;
 }
