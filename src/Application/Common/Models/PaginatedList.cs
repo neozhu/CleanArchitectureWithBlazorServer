b2dd05cd6386895ceac5ddf3d59eb7883b544c2e
hualin.zhu <neo.js.cn@gmail.com>
2023-06-14 07:20:00 +0800

    Update PaginatedList.cs

--- src/Application/Common/Models/PaginatedList.cs
+++ src/Application/Common/Models/PaginatedList.cs
@@ -3,24 +3,41 @@
 
 namespace CleanArchitecture.Blazor.Application.Common.Models;
 
-public class PaginatedList<T>
+public abstract class PaginatedList
 {
-    public List<T> Items { get; }
-    public int PageIndex { get; }
+    public PaginatedList(int count, int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        TotalCount = count;
+    }
+
+    public int PageNumber { get; }
     public int TotalPages { get; }
     public int TotalCount { get; }
 
-    public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+    public bool HasPreviousPage => this.PageNumber > 1;
+
+    public bool HasNextPage => this.PageNumber < this.TotalPages;
+
+    public static async Task<PaginatedList<T>> CreateAsync<T>(IQueryable<T> source, int pageNumber, int pageSize)
     {
-        PageIndex = pageIndex;
-        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-        TotalCount = count;
-        Items = items;
+        var count = await source.CountAsync();
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        return new PaginatedList<T>(items, count, pageNumber, pageSize);
     }
+}
+public class PaginatedList<T> : PaginatedList
+{
+    public List<T> Items { get; }
 
-    public bool HasPreviousPage => PageIndex > 1;
+    public PaginatedList(
+        List<T> items,
+        int count,
+        int pageNumber,
+        int pageSize) : base(count, pageNumber, pageSize) => Items = items;
 
-    public bool HasNextPage => PageIndex < TotalPages;
 
     public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
     {
@@ -30,3 +47,25 @@ public class PaginatedList<T>
         return new PaginatedList<T>(items, count, pageIndex, pageSize);
     }
 }
+
+//public class PaginatedList<T>
+//{
+//    public List<T> Items { get; }
+//    public int PageIndex { get; }
+//    public int TotalPages { get; }
+//    public int TotalCount { get; }
+
+//    public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+//    {
+//        PageIndex = pageIndex;
+//        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+//        TotalCount = count;
+//        Items = items;
+//    }
+
+//    public bool HasPreviousPage => PageIndex > 1;
+
+//    public bool HasNextPage => PageIndex < TotalPages;
+
+    
+//}
