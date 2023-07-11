bc5a84f189b8872cabe80e3260c3ee23efa775da
neo.zhu <new163@163.com>
2023-07-11 13:25:10 +0800

    Update UserNameConstants.cs

--- src/Application/Constants/User/UserNameConstants.cs
+++ src/Application/Constants/User/UserNameConstants.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace CleanArchitecture.Blazor.Application.Constants.User;
-
 public abstract class UserName
 {
     public const string Administrator = nameof(Administrator);
