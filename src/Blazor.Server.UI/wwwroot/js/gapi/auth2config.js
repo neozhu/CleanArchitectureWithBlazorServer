2183e7135c0046e3b7fb943687b17639e62cfc4b
Madhusudhan V <madhusudhan.v@live.in>
2023-08-01 19:06:43 +0530

    c

--- src/Blazor.Server.UI/wwwroot/js/gapi/auth2config.js
+++ src/Blazor.Server.UI/wwwroot/js/gapi/auth2config.js
@@ -1,7 +1,7 @@
 const auth2Config = {
     discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
     // clientId and scope are optional if auth is not required.
-    clientId: '225830526514-g0gh8qk11s945ne0e95b4mqbofdh6n1m.apps.googleusercontent.com',
+    clientId: 'zxczxczxczxcxzcxzcxzcz.apps.googleusercontent.com',
     scopes: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
     scope: 'https://www.googleapis.com/auth/userinfo.email',
     access_token: 'access_token'
