a3f26b3e4aefafb6da8925cbd4945d7acce43abf
Madhusudhan V <madhusudhan.v@live.in>
2023-08-21 11:01:40 +0530

    UpdateRolesAsyncWithTenantId completed

--- src/Infrastructure/Extensions/UserManagerExtensions.cs
+++ src/Infrastructure/Extensions/UserManagerExtensions.cs
@@ -4,10 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CleanArchitecture.Blazor.Domain.Entities;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace CleanArchitecture.Blazor.Domain.Identity;
+
 public static class UserManagerExtensions
 {
+    public static async Task<int> RemoveFromRolesAsyncWithTenantName(
+       string userId,
+       string tenantName, IApplicationDbContext context,
+       params string[] roles)
+    {
+        var tenant = await context.Tenants.FirstAsync(x => x.Name == tenantName);
+        return tenant != null ? await RemoveFromRolesAsyncWithTenantId(userId, tenant.Id, context, roles) : 0;
+    }
+    public static async Task<int> RemoveFromRolesAsyncWithTenantId(
+        string userId,
+        string tenantId, IApplicationDbContext context,
+        params string[] roles)
+    {
+        var itemsToRemove = context.UserRoles.Where(item => item.UserId == userId && item.TenantId == tenantId &&
+        context.Roles.Where(r => roles.Contains(r.Name)).Contains(item.Role));
+        context.UserRoles.RemoveRange(itemsToRemove);
+        return await context.SaveChangesAsync();
+    }
+
+    public static async Task<int> UpdateRolesAsyncWithTenantId(
+    string userId,
+    string tenantId, IApplicationDbContext context,
+    params string[] newSelectedRoles)
+    {
+        //todo we can check for tenanttype if it roles exists or not kind of more check
+        var existingRoles = await context.UserRoles.Where(item => item.UserId == userId && item.TenantId == tenantId).Select(x => new { UserRole = x, x.RoleId, x.Role.Name }).ToListAsync();
+
+        var toAdd = newSelectedRoles.Except(existingRoles.Select(x => x.Name));
+        var toRemove = existingRoles.Where(e => !newSelectedRoles.ToList().Contains(e.Name!)).ToList();
+
+        if (toAdd.Any())
+        {
+            var toAddList = new List<ApplicationUserRole>();
+            var toAddRoles = await context.Roles.Where(item => toAdd.Contains(item.Name)).Select(x => x.Id).ToListAsync();
+            toAddRoles.ForEach(t => toAddList.Add(new ApplicationUserRole() { UserId = userId, TenantId = tenantId, RoleId = t }));
+            await context.UserRoles.AddRangeAsync(toAddList);
+            Console.WriteLine($"Adding {toAdd.Count()} rules({toAdd})");
+        }
+
+        if (toRemove.Any())
+        {
+            context.UserRoles.RemoveRange(toRemove.Select(x => x.UserRole));
+            Console.WriteLine($"Removing {toRemove.Count()} rules({toAdd})");
+        }
+
+        if (toAdd.Any() || toRemove.Any())
+        {
+            var result = await context.SaveChangesAsync();
+            Console.WriteLine($"Role changes count:{result},Add:{toAdd.Count()},Remove:{toRemove.Count()}");
+        }
+        Console.WriteLine($"Not Role changes");
+        return 0;
+    }
+
+
     public static async Task<int> AddToRolesAsyncWithTenantId(
         string userId,
         string tenantId, IApplicationDbContext context,
@@ -31,10 +89,25 @@ public static class UserManagerExtensions
     {
 
         var tenant = await context.Tenants.FirstAsync(x => x.Name == tenantName);
-        if (tenant != null)
-        {
-            return await AddToRolesAsyncWithTenantId(userId, tenant.Id, context, roles);
-        }
-        return 0;
+        return tenant != null ? await AddToRolesAsyncWithTenantId(userId, tenant.Id, context, roles) : 0;
     }
+
+    /*
+   #region PleaseUseWithTenantMethods
+   static string _message = "Please use AddToRolesAsyncWithTenantId/Name or RemoveFromRolesAsyncWithTenantId/Name instead of this";
+   public static Exception PleaseUseWithTenantMethods()
+   {
+       Console.WriteLine(_message);
+       return new Exception(_message);
+   }
+   public static int AddToRoles(this CustomUserManager usermanager,
+      ApplicationUser user, params string[] roles) => throw PleaseUseWithTenantMethods();
+   public static async Task<int> AddToRolesAsync(this CustomUserManager usermanager,
+      ApplicationUser user, params string[] roles) => throw PleaseUseWithTenantMethods();
+
+   public static int RemoveFromRoles(this CustomUserManager usermanager,
+      ApplicationUser user, params string[] roles) => throw PleaseUseWithTenantMethods();
+   public static async Task<int> RemoveFromRolesAsync(this CustomUserManager usermanager,
+      ApplicationUser user, params string[] roles) => throw PleaseUseWithTenantMethods();
+   #endregion */
 }
