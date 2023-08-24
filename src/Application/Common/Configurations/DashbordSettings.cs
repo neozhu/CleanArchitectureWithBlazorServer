398df606b02ef7026fc01cc95625219528d58929
Bram1903 <70259613+Bram1903@users.noreply.github.com>
2023-08-24 11:19:40 +0200

    Add configurable loading screen duration
    A change was made to the App.razor, DashboardSettings.cs, and appsettings.json files to add a configurable loading screen duration setting in the application. Now, users can adjust the duration of the loading screen according to their preferences. This change makes the application more customizable and user-friendly.

--- src/Application/Common/Configurations/DashbordSettings.cs
+++ src/Application/Common/Configurations/DashbordSettings.cs
@@ -14,40 +14,49 @@ public class DashboardSettings
     public const string Key = nameof(DashboardSettings);
 
     /// <summary>
+    ///     Specifies whether to enable the loading screen
+    /// </summary>
+    public bool EnableLoadingScreen { get; set; } = true;
+
+    /// <summary>
+    ///     Specifies the duration of the loading screen in milliseconds
+    /// </summary>
+    public int LoadingScreenDuration { get; set; } = 3000;
+
+    /// <summary>
     ///     Current application version
     /// </summary>
     public string Version { get; set; } = "1.3.0";
-    
+
     /// <summary>
     ///     Application framework
     /// </summary>
-    
+
     public string App { get; set; } = "Blazor";
-    
+
     /// <summary>
     ///     The application name / title
     /// </summary>
     public string AppName { get; set; } = "Blazor Dashboard";
-    
+
     /// <summary>
     ///     Application framework including the version
     /// </summary>
-    
+
     public string AppFlavor { get; set; } = "Blazor .NET 7.0";
-    
+
     /// <summary>
     ///     Application .NET version
     /// </summary>
     public string AppFlavorSubscript { get; set; } = ".NET 7";
-    
+
     /// <summary>
     ///     The name of the company
     /// </summary>
     public string Company { get; set; } = "Company";
-    
+
     /// <summary>
     ///     Copyright watermark
     /// </summary>
     public string Copyright { get; set; } = "@2023 Copyright";
- 
 }
