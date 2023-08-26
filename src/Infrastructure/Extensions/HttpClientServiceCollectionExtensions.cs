0e38c424df7d43c8474ec667ebb0e330580afafb
hualin.zhu <neo.js.cn@gmail.com>
2023-08-26 18:09:48 +0800

    🍒 improve document OCR performance

--- src/Infrastructure/Extensions/HttpClientServiceCollectionExtensions.cs
+++ src/Infrastructure/Extensions/HttpClientServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ public static class HttpClientServiceCollectionExtensions
     public static void AddHttpClientService(this IServiceCollection services)
         => services.AddHttpClient("ocr", c =>
         {
-            c.BaseAddress = new Uri("https://paddleocr.i247365.net/predict/ocr_system");
-            c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            c.BaseAddress = new Uri("https://paddleocr.blazorserver.com/uploadocr");
+            c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("multipart/form-data"));
         }).AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(30)));
 }
