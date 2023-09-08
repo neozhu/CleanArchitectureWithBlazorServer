8765e27512022b8ea7007df044496fe759be20d1
Madhusudhan V <madhusudhan.v@live.in>
2023-09-08 12:16:30 +0530

    c

--- src/Application/Constants/Permission/Permissions.cs
+++ src/Application/Constants/Permission/Permissions.cs
@@ -381,12 +381,13 @@ public class RolePermissions
 }
 
 
-public class Perms
+public static class Perms
 {
     internal static r[] AllRoles = (RoleNamesEnum[])Enum.GetValues(typeof(r));
     public static List<r> CommonReadRoles = AllRoles.Where(e => e >= r.Pharmacist && e <= r.Hospital).ToList();
     public static List<string> CommonReadPermissions = RP(CommonReadRoles, p.Read);
     public static RolePermissions PatientPermissions = new(r.Patient, AddPermissions(r.Patient, CommonReadPermissions, p.CreateRestricted, p.UpdateRestricted, p.ReadRestricted));
+    
     public static RolePermissions PharmacistPermissions = new(r.Pharmacist,
         AddPermissions(r.Pharmacist, AddPermissions(r.Patient, CommonReadPermissions, p.Create, p.Update, p.Assign), p.Create, p.Update, p.Assign));
     public static RolePermissions PharmacyPermissions = new(r.Pharmacy,
@@ -394,10 +395,15 @@ public class Perms
         AddPermissions(r.Pharmacy, RolePermissions.GetPermissions(PharmacistPermissions), p.Assign,p.Create,p.Update),
         p.UnAssign));
 
+    public static RolePermissions DiagnosticPermissions = new(r.Diagnostic,
+        AddPermissions(r.Diagnostic, AddPermissions(r.Patient, CommonReadPermissions, p.Create, p.Update, p.Assign), p.Create, p.Update, p.Assign));
+    public static RolePermissions DiagnosticCenterPermissions = new(r.DiagnosticCenter,
+        AddPermissions(r.Diagnostic,
+        AddPermissions(r.DiagnosticCenter, RolePermissions.GetPermissions(DiagnosticPermissions), p.Assign, p.Create, p.Update),
+        p.UnAssign));
 
-    public static RolePermissions DiagnosticCenterPermissions = new(r.DiagnosticCenter, new RolePermissions(r.Diagnostic));
     public static RolePermissions HospitalRolePermissions = new(r.Hospital, new RolePermissions(r.HospitalAdmin, new RolePermissions(r.DoctorHOD, new RolePermissions(r.Doctor, new RolePermissions(r.DoctorAssistant, new RolePermissions(r.Nurse, new RolePermissions(r.ViewerHospital)))))));
-    public RolePermissions RootRolePermissions = new(r.RootAdmin, new RolePermissions(r.ElevateAdminGroup, new RolePermissions(r.ElevateAdminViewer, HospitalRolePermissions!)));
+    public static RolePermissions RootRolePermissions = new(r.RootAdmin, new RolePermissions(r.ElevateAdminGroup, new RolePermissions(r.ElevateAdminViewer, HospitalRolePermissions!)));
     public static string RP(r role, p perm = p.Read) { return role.ToString() + perm.ToString(); }
     public static List<string> RP(List<r> roles, p perm = p.Read)
     {
