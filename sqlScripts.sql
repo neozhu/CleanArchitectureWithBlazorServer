2ee63bed3a1da5ade69a26bf6805c9812cf30c2b
Madhusudhan V <madhusudhan.v@live.in>
2023-09-11 18:45:19 +0530

    c

--- sqlScripts.sql
+++ sqlScripts.sql
@@ -1,11 +1,12 @@
 use [9katthe]
+SELECT userid,RoleId,u.UserName,r.Name,ur.TenantId  FROM [AspNetUserRoles] ur left join AspNetUsers u on ur.UserId=u.Id left join AspNetRoles r on ur.RoleId=r.Id
 --SELECT *  FROM [Tenants]
 --SELECT *   FROM [AspNetRoles]
---SELECT *  FROM [AspNetUserRoles]
+SELECT *  FROM [AspNetUserRoles]
 --select * from [AspNetUsers]
 
 --update [AspNetUsers]
 --set TenantId='b7384612-5768-445a-bf62-aeddd039cf9a' where id='b76c06e9-a767-4824-835f-8b4e86b52c90'
 
-select * FROM [AspNetRoleClaims]
-select * from [AspNetUserClaims]
+--select * FROM [AspNetRoleClaims]
+--select * from [AspNetUserClaims]
