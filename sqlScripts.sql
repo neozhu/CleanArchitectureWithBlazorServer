3432d78b717d1fcae913fd5c79ad524139d3d21e
Madhusudhan V <madhusudhan.v@live.in>
2023-09-11 18:13:49 +0530

    sql trial scripts update

--- sqlScripts.sql
+++ sqlScripts.sql
@@ -1,12 +1,11 @@
-use [mainkatthe]
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT *  FROM [dbo].[Tenants]
-SELECT *   FROM [dbo].[AspNetRoles]
-SELECT *  FROM [dbo].[AspNetUserRoles]
-select * from [dbo].[AspNetUsers]
+use [9katthe]
+--SELECT *  FROM [Tenants]
+--SELECT *   FROM [AspNetRoles]
+--SELECT *  FROM [AspNetUserRoles]
+--select * from [AspNetUsers]
 
---update [dbo].[AspNetUsers]
+--update [AspNetUsers]
 --set TenantId='b7384612-5768-445a-bf62-aeddd039cf9a' where id='b76c06e9-a767-4824-835f-8b4e86b52c90'
 
---select * FROM [dbo].[AspNetRoleClaims]
+select * FROM [AspNetRoleClaims]
 select * from [AspNetUserClaims]
