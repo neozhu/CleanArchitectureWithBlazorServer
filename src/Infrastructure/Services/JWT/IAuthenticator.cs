e960eae715025d2b00c345f68369c46e560a3a9a
hualin.zhu <neo.js.cn@gmail.com>
2023-10-02 20:14:05 +0800

    add DefaultAuthenticator

---
+++ src/Infrastructure/Services/JWT/IAuthenticator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Blazor.Application.Features.Identity.Dto;
+
+namespace CleanArchitecture.Blazor.Infrastructure.Services.JWT;
+public interface IAuthenticator
+{
+    Task<ApplicationUserDto?> Authenticate(string username, string password);
+}
