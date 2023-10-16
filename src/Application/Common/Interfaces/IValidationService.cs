7163bbe3a91b97e33c4f5c98d5ed21006dc8b726
MatteoZamparini <matteozamparinidev@gmail.com>
2023-10-16 13:48:45 +0200

    Added ValidationService and sample use case

---
+++ src/Application/Common/Interfaces/IValidationService.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Internal;
+
+namespace CleanArchitecture.Blazor.Application.Common.Interfaces;
+public interface IValidationService
+{
+    Func<object, string, Task<IEnumerable<string>>> ValidateValue<TRequest>();
+    // NOTE: providing the model as parameter is not required,
+    // it's just easier to write and nicer to read in the blazor component
+    // instead of the explicit declaration of the model type
+    Func<object, string, Task<IEnumerable<string>>> ValidateValue<TRequest>(TRequest _);
+    Task<IDictionary<string, string[]>> ValidateAsync<TRequest>(TRequest model, CancellationToken cancellationToken = default);
+    Task<IDictionary<string, string[]>> ValidateAsync<TRequest>(TRequest model, Action<ValidationStrategy<TRequest>> options, CancellationToken cancellationToken = default);
+    Task<IEnumerable<string>> ValidatePropertyAsync<TRequest>(TRequest model, string propertyName, CancellationToken cancellationToken = default);
+
+}
---
+++ src/Application/Services/Validation/ValidationService.cs
@@ -0,0 +1,61 @@
+﻿using CleanArchitecture.Blazor.Application.Common.Helper;
+using FluentValidation.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Blazor.Application.Services.Validation;
+public class ValidationService : IValidationService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    // in order to keep the service as generic as possible 
+    // validators are provided by type only when required,
+    // an alternative would be creating a IValidationService<TRequest>
+    // similar to the ValidationBehaviour<TRequest, TResponse>
+    // but that would mean injecting a IValidationService for each
+    // type of model to validate in a page
+    public ValidationService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue<TRequest>()
+        => async (model, propertyName)
+        => await ValidatePropertyAsync((TRequest)model, propertyName);
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue<TRequest>(TRequest _)
+        => this.ValidateValue<TRequest>();
+
+    public async Task<IDictionary<string, string[]>> ValidateAsync<TRequest>(TRequest model, CancellationToken cancellationToken = default)
+    {
+        var validators = _serviceProvider.GetServices<IValidator<TRequest>>();
+
+        var context = new ValidationContext<TRequest>(model);
+
+        return validators != null
+            ? await ValidationHelper.ValidateAsync(validators, context, cancellationToken)
+            : new Dictionary<string, string[]>();
+    }
+
+    public async Task<IDictionary<string, string[]>> ValidateAsync<TRequest>(TRequest model, Action<ValidationStrategy<TRequest>> options, CancellationToken cancellationToken = default)
+    {
+        var validators = _serviceProvider.GetServices<IValidator<TRequest>>();
+
+        var context = ValidationContext<TRequest>
+            .CreateWithOptions(model, options);
+
+        return validators != null
+            ? await ValidationHelper.ValidateAsync(validators, context, cancellationToken)
+            : new Dictionary<string, string[]>();
+    }
+
+    public async Task<IEnumerable<string>> ValidatePropertyAsync<TRequest>(TRequest model, string propertyName, CancellationToken cancellationToken = default)
+    {
+        var validationResult = await ValidateAsync(model,
+            options =>
+            {
+                options.IncludeProperties(propertyName);
+            }, cancellationToken);
+
+        return validationResult.Where(x => x.Key == propertyName).SelectMany(x => x.Value);
+    }
+}
