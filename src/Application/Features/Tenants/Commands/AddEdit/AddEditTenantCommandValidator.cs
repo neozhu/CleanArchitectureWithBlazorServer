bcc78efdfc92d3adb55bdfcdaa44d6187ebd3a44
hualin.zhu <neo.js.cn@gmail.com>
2023-10-16 20:41:06 +0800

    ✨Refactor: Validation Logic using ValidationService

--- src/Application/Features/Tenants/Commands/AddEdit/AddEditTenantCommandValidator.cs
+++ src/Application/Features/Tenants/Commands/AddEdit/AddEditTenantCommandValidator.cs
@@ -12,11 +12,5 @@ public class AddEditTenantCommandValidator : AbstractValidator<AddEditTenantComm
             .NotEmpty();
     }
 
-    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-    {
-        var result =
-            await ValidateAsync(ValidationContext<AddEditTenantCommand>.CreateWithOptions((AddEditTenantCommand)model,
-                x => x.IncludeProperties(propertyName)));
-        return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
-    };
+    
 }
