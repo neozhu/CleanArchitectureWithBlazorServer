45863890eff9d9c19dd1c133c02856e7fe4bf8c8
hualin.zhu <neo.js.cn@gmail.com>
2023-10-25 19:31:06 +0800

    ✨ add RefreshToken(string refreshToken) method

--- src/Infrastructure/Services/JWT/DefaultAuthenticator.cs
+++ src/Infrastructure/Services/JWT/DefaultAuthenticator.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CleanArchitecture.Blazor.Application.Features.Identity.Dto;
 using CleanArchitecture.Blazor.Domain.Features.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CleanArchitecture.Blazor.Infrastructure.Services.JWT;
 public class DefaultAuthenticator : IAuthenticator
 {
     private readonly UserManager<ApplicationUser> _userManager;
-    public DefaultAuthenticator(IServiceScopeFactory scopeFactory)
+    private readonly ITokenValidator _tokenValidator;
+    private readonly IAccessTokenGenerator _tokenGenerator;
+
+    public DefaultAuthenticator(IServiceScopeFactory scopeFactory, ITokenValidator tokenValidator, IAccessTokenGenerator tokenGenerator)
     {
         var scope = scopeFactory.CreateScope();
         _userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        _tokenValidator = tokenValidator;
+        _tokenGenerator = tokenGenerator;
     }
     public async Task<ApplicationUser?> Authenticate(string username, string password)
     {
@@ -33,5 +41,23 @@ public class DefaultAuthenticator : IAuthenticator
         }
 
         return user;
+    }
+
+    public async Task<string> RefreshToken(string refreshToken)
+    {
+        TokenValidationResult validationResult = await _tokenValidator.ValidateTokenAsync(refreshToken!);
+        if (!validationResult.IsValid)
+        {
+            return string.Empty;
+        }
+        JwtSecurityToken? jwt = validationResult.SecurityToken as JwtSecurityToken;
+        string userId = jwt!.Claims.First(claim => claim.Type == "id").Value;
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return string.Empty;
+        }
+        string accessToken =await _tokenGenerator.GenerateAccessToken(user);
+        return accessToken;
     }
 }
