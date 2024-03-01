4ec0459feb72c4c4db5bdf41b2e7ab9107ac6d0d
hualin.zhu <neo.js.cn@gmail.com>
2024-03-01 08:58:23 +0800

    ✨ Upgrade NuGet Package Versions

--- src/Application/Common/ExceptionHandlers/DbExceptionHandler.cs
+++ src/Application/Common/ExceptionHandlers/DbExceptionHandler.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
-
-namespace CleanArchitecture.Blazor.Application.Common.ExceptionHandlers;
+﻿namespace CleanArchitecture.Blazor.Application.Common.ExceptionHandlers;
 
 public class
     DbExceptionHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException>
@@ -27,31 +25,33 @@ public class
         IList<string> errors = new List<string>();
         if (exception.InnerException != null
             && exception.InnerException != null
-            && exception.InnerException is SqlException sqlException
            )
-            switch (sqlException.Number)
+            //switch (sqlException.Number)
+            //{
+            //    case 2627: // Unique constraint error
+            //        errors.Add(
+            //            "A Unique Constraint Error Has Occured While Updating the record! Duplicate Record cannot be inserted in the System.");
+            //        break;
+            //    case 544
+            //        : // Cannot insert explicit value for identity column in table 'Departments' when IDENTITY_INSERT is set to OFF
+            //        errors.Add(
+            //            "Cannot insert explicit value for identity column in the system when the id is set to OFF");
+            //        break;
+            //    case 547: // Constraint check violation, Conflict in the database
+            //        errors.Add("A Constraint Check violation Error Has Occured While Updating the record(s)!");
+            //        break;
+            //    case 2601
+            //        : // Duplicated key row error // Constraint violation exception // A custom exception of yours for concurrency issues           
+            //        errors.Add("A Duplicate Key Error Has Occured While Updating the record(s)!");
+            //        break;
+            //    case 201: // Procedure missing parameters            
+            //        errors.Add("A Missing Parameter has led to Error  While Creating the record(s)!");
+            //        break;
+            //}
+            foreach (var result in exception.Entries)
             {
-                case 2627: // Unique constraint error
-                    errors.Add(
-                        "A Unique Constraint Error Has Occured While Updating the record! Duplicate Record cannot be inserted in the System.");
-                    break;
-                case 544
-                    : // Cannot insert explicit value for identity column in table 'Departments' when IDENTITY_INSERT is set to OFF
-                    errors.Add(
-                        "Cannot insert explicit value for identity column in the system when the id is set to OFF");
-                    break;
-                case 547: // Constraint check violation, Conflict in the database
-                    errors.Add("A Constraint Check violation Error Has Occured While Updating the record(s)!");
-                    break;
-                case 2601
-                    : // Duplicated key row error // Constraint violation exception // A custom exception of yours for concurrency issues           
-                    errors.Add("A Duplicate Key Error Has Occured While Updating the record(s)!");
-                    break;
-                case 201: // Procedure missing parameters            
-                    errors.Add("A Missing Parameter has led to Error  While Creating the record(s)!");
-                    break;
+                errors.Add($"A DbUpdateException was caught while saving changes. Type: {result.Entity.GetType().Name} was part of the problem. ");
             }
-
 
         return errors.ToArray();
     }
