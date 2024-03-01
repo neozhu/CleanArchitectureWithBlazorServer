e1e15bf953ff95866f49a7f7caf392b1553a88da
hualin.zhu <neo.js.cn@gmail.com>
2024-03-01 08:42:09 +0800

    ✨ create index for logger table

--- src/Migrators/Migrators.MSSQL/Migrations/20240301003916_create_index_logger.Designer.cs
+++ src/Migrators/Migrators.MSSQL/Migrations/20240301003916_create_index_logger.Designer.cs
@@ -4,6 +4,7 @@ using CleanArchitecture.Blazor.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchitecture.Blazor.Migrators.MSSQL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240301003916_create_index_logger")]
+    partial class create_index_logger
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
