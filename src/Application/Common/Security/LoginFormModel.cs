5c2b05efbf3690c86568942ba6ea1c90c49f2276
hualin.zhu <neo.js.cn@gmail.com>
2024-03-24 20:13:47 +0800

    ✨ add mutil language resource

--- src/Application/Common/Security/LoginFormModel.cs
+++ src/Application/Common/Security/LoginFormModel.cs
@@ -8,10 +8,10 @@ public class LoginFormModel
 {
     [Required(ErrorMessage = "User name cannot be empty")]
     [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
-    public string? UserName { get; set; } = "";
+    public string UserName { get; set; } = "";
     [Required(ErrorMessage = "Password cannot be empty")]
     [StringLength(30, ErrorMessage = "Password must be at least 6 characters long.", MinimumLength = 6)]
-    public string? Password { get; set; } = "";
+    public string Password { get; set; } = "";
     public bool RememberMe { get; set; } = false;
 }
 
