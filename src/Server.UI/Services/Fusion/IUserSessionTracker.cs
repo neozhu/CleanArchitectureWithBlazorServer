524237f517e3ffe7e0ef2f90f9d23c2d0bd230b1
neozhu <new163@163.com>
2024-04-18 09:30:26 +0800

    🐛 fixed

--- src/Server.UI/Services/Fusion/IUserSessionTracker.cs
+++ src/Server.UI/Services/Fusion/IUserSessionTracker.cs
@@ -5,8 +5,8 @@ namespace CleanArchitecture.Blazor.Server.UI.Services.Fusion;
 
 public interface IUserSessionTracker: IComputeService
 {
-    Task AddUser(string pagecomponent, string userName, CancellationToken cancellationToken = default);
-    Task RemoveUser(string pagecomponent, string userName, CancellationToken cancellationToken = default);
+    Task AddUserSession(string pageComponent, string userName, CancellationToken cancellationToken = default);
+    Task RemoveUserSession(string pageComponent, string userName, CancellationToken cancellationToken = default);
     [ComputeMethod]
     Task<(string PageComponent, string[] UserSessions)[]> GetUserSessions( CancellationToken cancellationToken = default);
 }
--- src/Server.UI/Services/Fusion/UserSessionTracker.cs
+++ src/Server.UI/Services/Fusion/UserSessionTracker.cs
@@ -1,61 +1,56 @@
 ﻿using System.Collections.Immutable;
+using Stl.Fusion;
 
 namespace CleanArchitecture.Blazor.Server.UI.Services.Fusion;
 
 public class UserSessionTracker : IUserSessionTracker
 {
-    private readonly object _lock = new object();
+
     private volatile ImmutableDictionary<string, ImmutableHashSet<string>> _pageUserSessions = ImmutableDictionary<string, ImmutableHashSet<string>>.Empty;
 
-    public async Task AddUser(string pageComponent, string userName, CancellationToken cancellationToken = default)
+    public async Task AddUserSession(string pageComponent, string userName, CancellationToken cancellationToken = default)
     {
-        lock (_lock)
+        if (_pageUserSessions.TryGetValue(pageComponent, out var existingUsers))
         {
-            // 尝试获取指定页面的用户集合
-            if (_pageUserSessions.TryGetValue(pageComponent, out var existingUsers))
+            if (!existingUsers.Contains(userName))
             {
-                // 如果用户名不存在于当前集合，更新集合
-                if (!existingUsers.Contains(userName))
-                {
-                    var updatedUsers = existingUsers.Add(userName);
-                    _pageUserSessions = _pageUserSessions.SetItem(pageComponent, updatedUsers);
-                }
+                var updatedUsers = existingUsers.Add(userName);
+                _pageUserSessions = _pageUserSessions.SetItem(pageComponent, updatedUsers);
+            }
+        }
+        else
+        {
+            _pageUserSessions = _pageUserSessions.Add(pageComponent, ImmutableHashSet.Create(userName));
+        }
+        using (Computed.Invalidate())
+        {
+            _ = await GetUserSessions(cancellationToken);
+        }
+    }
+
+    public virtual  Task<(string PageComponent, string[] UserSessions)[]> GetUserSessions(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_pageUserSessions.Select(kvp => (kvp.Key, kvp.Value.ToArray())).ToArray());
+    }
+
+    public async Task RemoveUserSession(string pageComponent, string userName, CancellationToken cancellationToken = default)
+    {
+        if (_pageUserSessions.TryGetValue(pageComponent, out var users) && users.Contains(userName))
+        {
+            var updatedUsers = users.Remove(userName);
+            if (updatedUsers.IsEmpty)
+            {
+                _pageUserSessions = _pageUserSessions.Remove(pageComponent);
             }
             else
             {
-                // 页面组件不存在，创建新的用户集合
-                _pageUserSessions = _pageUserSessions.Add(pageComponent, ImmutableHashSet.Create(userName));
+                _pageUserSessions = _pageUserSessions.SetItem(pageComponent, updatedUsers);
             }
         }
 
-        // 异步获取最新的用户会话列表，这里不需要等待返回结果，因此使用 _ = 可以忽略返回的任务
-        _ =await GetUserSessions(cancellationToken);
-    }
-
-    public virtual async Task<(string PageComponent, string[] UserSessions)[]> GetUserSessions(CancellationToken cancellationToken = default)
-    {
-        return _pageUserSessions.Select(kvp => (kvp.Key, kvp.Value.ToArray())).ToArray();
-    }
-
-    public async Task RemoveUser(string pageComponent, string userName, CancellationToken cancellationToken = default)
-    {
-        lock (_lock)
+        using (Computed.Invalidate())
         {
-            if (_pageUserSessions.TryGetValue(pageComponent, out var users) && users.Contains(userName))
-            {
-                var updatedUsers = users.Remove(userName);
-                if (updatedUsers.IsEmpty)
-                {
-                    _pageUserSessions = _pageUserSessions.Remove(pageComponent);
-                }
-                else
-                {
-                    _pageUserSessions = _pageUserSessions.SetItem(pageComponent, updatedUsers);
-                }
-            }
+            _ = await GetUserSessions(cancellationToken);
         }
-
-        // 异步获取最新的用户会话列表，这里不需要等待完成，因此使用 _ = 可以忽略返回的任务
-        _ = await GetUserSessions(cancellationToken);
     }
 }
