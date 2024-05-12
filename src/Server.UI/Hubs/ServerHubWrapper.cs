3ad628cac07f492116538b66233a475c2b4d1c6d
hualin.zhu <neo.js.cn@gmail.com>
2024-05-12 16:53:20 +0800

    🪶 clean project remove server project

---
+++ src/Server.UI/Hubs/ServerHubWrapper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CleanArchitecture.Blazor.Server.UI.Hubs;
+
+public class ServerHubWrapper : IApplicationHubWrapper
+{
+    private readonly IHubContext<ServerHub, ISignalRHub> _hubContext;
+
+    public ServerHubWrapper(IHubContext<ServerHub, ISignalRHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task JobStarted(string message)
+    {
+        await _hubContext.Clients.All.Start(message);
+    }
+
+    public async Task JobCompleted(string message)
+    {
+        await _hubContext.Clients.All.Completed(message);
+    }
+}
---
+++ src/Server.UI/Middlewares/HangfireDashboardAuthorizationFilter.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire.Dashboard;
+
+namespace CleanArchitecture.Blazor.Server.UI.Middlewares;
+
+public class HangfireDashboardAsyncAuthorizationFilter : IDashboardAsyncAuthorizationFilter
+{
+    public Task<bool> AuthorizeAsync(DashboardContext context)
+    {
+        return Task.FromResult(true);
+    }
+}
+
+public class HangfireDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+{
+    public bool Authorize(DashboardContext context)
+    {
+        return true;
+    }
+}
