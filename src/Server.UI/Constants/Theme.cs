f899c0ac9020ecf6c63c201aedabfef45f0fc37d
hualin.zhu <neo.js.cn@gmail.com>
2024-06-07 21:41:00 +0800

    Update Theme.cs

--- src/Server.UI/Constants/Theme.cs
+++ src/Server.UI/Constants/Theme.cs
@@ -9,7 +9,7 @@ public class Theme
             PaletteLight = new PaletteLight
             {
                 Primary = "#5052ba",
-                Secondary = "#80838b",
+                Secondary = "rgba(237, 75, 66,1)",
                 Success = "#24ab5e",
                 Info = "#4099f3",
                 Warning = "#f0c42b",
@@ -36,7 +36,7 @@ public class Theme
             PaletteDark = new PaletteDark
             {
                 Primary = "#5052ba",
-                Secondary = "#80838b",
+                Secondary = "rgba(237, 75, 66,1)",
                 Success = "#24ab5e",
                 Info = "#4099f3",
                 Warning = "#f0c42b",
