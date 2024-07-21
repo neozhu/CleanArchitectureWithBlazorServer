3b2173e0b47ca7a029133ff34af178e1e2a26966
hualin.zhu <neo.js.cn@gmail.com>
2024-07-21 13:20:02 +0800

    Regular update

---
+++ src/Server.UI/Services/JsInterop/Swiper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace CleanArchitecture.Blazor.Server.UI.Services.JsInterop;
+
+public sealed class Swiper
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public Swiper(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task<ValueTask> Initialize(string elment,bool reverse=false)
+    {
+        var jsmodule = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/carousel.js");
+        return jsmodule.InvokeVoidAsync("initializeSwiper", elment, reverse);
+    }
+}
