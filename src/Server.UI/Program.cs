80c4c36d2d2d8a3fcbded8c25b9ba87a75d29cfa
hualin.zhu <neo.js.cn@gmail.com>
2024-09-22 20:40:02 +0800

    HostExtensions

--- src/Server.UI/Program.cs
+++ src/Server.UI/Program.cs
@@ -1,6 +1,5 @@
 ﻿using CleanArchitecture.Blazor.Application;
 using CleanArchitecture.Blazor.Infrastructure;
-using CleanArchitecture.Blazor.Infrastructure.Persistence;
 using CleanArchitecture.Blazor.Server.UI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,13 +16,6 @@ var app = builder.Build();
 
 app.ConfigureServer(builder.Configuration);
 
-if (app.Environment.IsDevelopment())
-    // Initialise and seed database
-    using (var scope = app.Services.CreateScope())
-    {
-        var initializer = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
-        await initializer.InitialiseAsync().ConfigureAwait(false);
-        await initializer.SeedAsync().ConfigureAwait(false);
-    }
+await app.InitializeDatabaseAsync().ConfigureAwait(false);
 
 await app.RunAsync().ConfigureAwait(false);
