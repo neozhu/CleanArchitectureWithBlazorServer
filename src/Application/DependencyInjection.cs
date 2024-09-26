cbf757ffd088dc6449aac85e7c852f020222f632
hualin <hualin1.zhu@voith.com>
2024-09-26 13:07:42 +0800

    commit

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -14,13 +14,13 @@ public static class DependencyInjection
     {
         services.AddAutoMapper(config => { config.AddMaps(Assembly.GetExecutingAssembly()); });
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IRequestExceptionHandler<,,>), typeof(DbExceptionHandler<,,>));
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             config.NotificationPublisher = new ParallelNoWaitPublisher();
             config.AddRequestPreProcessor(typeof(IRequestPreProcessor<>), typeof(ValidationPreProcessor<>));
             config.AddOpenBehavior(typeof(PerformanceBehaviour<,>));
-            config.AddOpenBehavior(typeof(RequestExceptionProcessorBehavior<,>));
             config.AddOpenBehavior(typeof(MemoryCacheBehaviour<,>));
             config.AddOpenBehavior(typeof(FusionCacheBehaviour<,>));
             config.AddOpenBehavior(typeof(CacheInvalidationBehaviour<,>));
