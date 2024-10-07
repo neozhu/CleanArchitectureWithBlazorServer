59fe6d1dee9b1af20cf9f591d933dd7044dc6c5d
hualin.zhu <neo.js.cn@gmail.com>
2024-10-07 19:58:49 +0800

    LanguageAutocomplete ,TimeZoneAutocomplete

---
+++ src/Server.UI/Components/Autocompletes/TimeZoneAutocomplete.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchitecture.Blazor.Application.Features.PicklistSets.DTOs;
+
+namespace CleanArchitecture.Blazor.Server.UI.Components.Autocompletes;
+
+public class TimeZoneAutocomplete<T> : MudAutocomplete<string>
+{
+    private List<TimeZoneInfo> TimeZones { get; set; }= TimeZoneInfo.GetSystemTimeZones().ToList();
+    public TimeZoneAutocomplete()
+    {
+        SearchFunc = SearchFunc_;
+        Dense = true;
+        ResetValueOnEmptyText = true;
+        ToStringFunc = x =>
+        {
+            var timeZone = TimeZones.FirstOrDefault(tz => tz.Id == x);
+            return timeZone != null ? timeZone.DisplayName : x;
+        };
+    }
+
+    private Task<IEnumerable<string>> SearchFunc_(string value, CancellationToken cancellation = default)
+    {
+        return string.IsNullOrEmpty(value)
+            ? Task.FromResult(TimeZones.Select(tz => tz.Id).AsEnumerable())
+            : Task.FromResult(TimeZones
+                .Where(tz => Contains(tz, value))
+                .Select(tz => tz.Id));
+    }
+
+    private static bool Contains(TimeZoneInfo timeZone, string value)
+    {
+        return timeZone.DisplayName.Contains(value, StringComparison.InvariantCultureIgnoreCase) ||
+               timeZone.Id.Contains(value, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
