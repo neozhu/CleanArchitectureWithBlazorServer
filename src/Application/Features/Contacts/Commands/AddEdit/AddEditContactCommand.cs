27f2f15a78ced563ae671ec1ced636ee1e5cbdb1
hualin <hualin1.zhu@voith.com>
2024-11-06 11:20:14 +0800

    wip

--- src/Application/Features/Contacts/Commands/AddEdit/AddEditContactCommand.cs
+++ src/Application/Features/Contacts/Commands/AddEdit/AddEditContactCommand.cs
@@ -3,63 +3,58 @@
 
 using CleanArchitecture.Blazor.Application.Features.Contacts.DTOs;
 using CleanArchitecture.Blazor.Application.Features.Contacts.Caching;
+using CleanArchitecture.Blazor.Application.Features.Contacts.Mappers;
 namespace CleanArchitecture.Blazor.Application.Features.Contacts.Commands.AddEdit;
 
-public class AddEditContactCommand: ICacheInvalidatorRequest<Result<int>>
+public class AddEditContactCommand : ICacheInvalidatorRequest<Result<int>>
 {
-      [Description("Id")]
-      public int Id { get; set; }
-          [Description("Name")]
-    public string? Name {get;set;} 
+    [Description("Id")]
+    public int Id { get; set; }
+    [Description("Name")]
+    public string? Name { get; set; }
     [Description("Description")]
-    public string? Description {get;set;} 
+    public string? Description { get; set; }
     [Description("Email")]
-    public string? Email {get;set;} 
+    public string? Email { get; set; }
     [Description("Phone number")]
-    public string? PhoneNumber {get;set;} 
+    public string? PhoneNumber { get; set; }
     [Description("Country")]
-    public string? Country {get;set;} 
+    public string? Country { get; set; }
 
 
-      public string CacheKey => ContactCacheKey.GetAllCacheKey;
-      public CancellationTokenSource? SharedExpiryTokenSource => ContactCacheKey.GetOrCreateTokenSource();
+    public string CacheKey => ContactCacheKey.GetAllCacheKey;
+    public CancellationTokenSource? SharedExpiryTokenSource => ContactCacheKey.GetOrCreateTokenSource();
 
     private class Mapping : Profile
     {
         public Mapping()
         {
-            CreateMap<ContactDto,AddEditContactCommand>(MemberList.None);
-            CreateMap<AddEditContactCommand,Contact>(MemberList.None);
-         
+            CreateMap<ContactDto, AddEditContactCommand>(MemberList.None);
+            CreateMap<AddEditContactCommand, Contact>(MemberList.None);
+
         }
     }
 }
 
-    public class AddEditContactCommandHandler : IRequestHandler<AddEditContactCommand, Result<int>>
+public class AddEditContactCommandHandler : IRequestHandler<AddEditContactCommand, Result<int>>
+{
+    private readonly IApplicationDbContext _context;
+    public AddEditContactCommandHandler(
+        IApplicationDbContext context
+        )
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IStringLocalizer<AddEditContactCommandHandler> _localizer;
-        public AddEditContactCommandHandler(
-            IApplicationDbContext context,
-            IStringLocalizer<AddEditContactCommandHandler> localizer,
-            IMapper mapper
-            )
-        {
-            _context = context;
-            _localizer = localizer;
-            _mapper = mapper;
-        }
+        _context = context;
+    }
     public async Task<Result<int>> Handle(AddEditContactCommand request, CancellationToken cancellationToken)
     {
         if (request.Id > 0)
         {
-            var item = await _context.Contacts.FindAsync(request.Id , cancellationToken);
+            var item = await _context.Contacts.FindAsync(request.Id, cancellationToken);
             if (item == null)
             {
                 return await Result<int>.FailureAsync($"Contact with id: [{request.Id}] not found.");
             }
-            item = _mapper.Map(request, item);
+            ContactMapper.MapToExisting(request,item);
             // raise a update domain event
             item.AddDomainEvent(new ContactUpdatedEvent(item));
             await _context.SaveChangesAsync(cancellationToken);
@@ -67,7 +62,7 @@ public class AddEditContactCommand: ICacheInvalidatorRequest<Result<int>>
         }
         else
         {
-            var item = _mapper.Map<Contact>(request);
+            var item = ContactMapper.Map(request);
             // raise a create domain event
             item.AddDomainEvent(new ContactCreatedEvent(item));
             _context.Contacts.Add(item);
@@ -75,5 +70,5 @@ public class AddEditContactCommand: ICacheInvalidatorRequest<Result<int>>
             return await Result<int>.SuccessAsync(item.Id);
         }
     }
-    }
+}
 
