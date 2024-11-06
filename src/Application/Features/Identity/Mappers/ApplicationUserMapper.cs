62801c4dc4b950758b319c02c1242f11d1a77ce2
hualin <hualin1.zhu@voith.com>
2024-11-06 16:33:39 +0800

    Update ApplicationUserMapper.cs

--- src/Application/Features/Identity/Mappers/ApplicationUserMapper.cs
+++ src/Application/Features/Identity/Mappers/ApplicationUserMapper.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchitecture.Blazor.Application.Features.Identity.DTOs;
+﻿using CleanArchitecture.Blazor.Application.Features.Documents.DTOs;
+using CleanArchitecture.Blazor.Application.Features.Identity.DTOs;
 using CleanArchitecture.Blazor.Application.Features.Tenants.Mappers;
 using CleanArchitecture.Blazor.Domain.Identity;
 using Riok.Mapperly.Abstractions;
@@ -10,41 +11,13 @@ namespace CleanArchitecture.Blazor.Application.Features.Identity.Mappers;
 [UseStaticMapper(typeof(TenantMapper))]
 public static partial class ApplicationUserMapper
 {
-    [MapPropertyFromSource(nameof(ApplicationUser.CreatedByUser), Use = nameof(MapWithoutRelatedProperties))]
-    [MapPropertyFromSource(nameof(ApplicationUser.LastModifiedByUser), Use = nameof(MapWithoutRelatedProperties))]
-    [MapPropertyFromSource(nameof(ApplicationUser.Superior), Use = nameof(MapWithoutRelatedProperties))]
+    [MapPropertyFromSource(nameof(ApplicationUser.CreatedByUser), Use = nameof(mapWithoutRelatedProperties))]
+    [MapPropertyFromSource(nameof(ApplicationUser.LastModifiedByUser), Use = nameof(mapWithoutRelatedProperties))]
+    [MapPropertyFromSource(nameof(ApplicationUser.Superior), Use = nameof(mapWithoutRelatedProperties))]
     public static partial ApplicationUserDto ToApplicationUserDto(ApplicationUser user);
 
-    // Custom mapping method to map only direct properties, excluding navigation properties
-    public static ApplicationUserDto MapWithoutRelatedProperties(ApplicationUser user)
-    {
-        if (user == null)
-            return null;
-
-        return new ApplicationUserDto
-        {
-            Id = user.Id,
-            UserName = user.UserName,
-            DisplayName = user.DisplayName,
-            Provider = user.Provider,
-            TenantId = user.TenantId,
-            ProfilePictureDataUrl = user.ProfilePictureDataUrl,
-            Email = user.Email,
-            PhoneNumber = user.PhoneNumber,
-            SuperiorId = user.SuperiorId,
-            IsActive = user.IsActive,
-            IsLive = user.IsLive,
-            EmailConfirmed = user.EmailConfirmed,
-            LockoutEnd = user.LockoutEnd,
-            TimeZoneId = user.TimeZoneId,
-            LanguageCode = user.LanguageCode,
-            LastModified = user.LastModified,
-            LastModifiedBy = user.LastModifiedBy,
-            Created = user.Created,
-            CreatedBy = user.CreatedBy
-            // Ignoring navigation properties like Superior, CreatedByUser, LastModifiedByUser
-        };
-    }
-
-     
+    [MapperIgnoreSource(nameof(ApplicationUser.CreatedByUser))]
+    [MapperIgnoreSource(nameof(ApplicationUser.LastModifiedByUser))]
+    [MapperIgnoreSource(nameof(ApplicationUser.Superior))]
+    private static partial ApplicationUserDto mapWithoutRelatedProperties(ApplicationUser user);
 }
