c9ff9a91e4f8790ab8de55d04502067eb9b747e2
hualin.zhu <neo.js.cn@gmail.com>
2024-11-06 19:57:48 +0800

    add AuditTrailMapper

--- src/Application/Features/AuditTrails/DTOs/AuditTrailDto.cs
+++ src/Application/Features/AuditTrails/DTOs/AuditTrailDto.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using CleanArchitecture.Blazor.Application.Common.Interfaces.Serialization;
@@ -10,34 +10,15 @@ namespace CleanArchitecture.Blazor.Application.Features.AuditTrails.DTOs;
 public class AuditTrailDto
 {
     [Description("Id")] public int Id { get; set; }
-
     [Description("User Id")] public string? UserId { get; set; }
-
     [Description("Audit Type")] public AuditType? AuditType { get; set; }
-
     [Description("Table Name")] public string? TableName { get; set; }
-
     [Description("Created DateTime")] public DateTime DateTime { get; set; }
-
     [Description("Old Values")] public Dictionary<string, object?>? OldValues { get; set; }
-
     [Description("New Values")] public Dictionary<string, object?>? NewValues { get; set; }
-
     [Description("Affected Columns")] public List<string>? AffectedColumns { get; set; }
-
     [Description("Primary Key")] public string PrimaryKey { get; set; } = default!;
-
     [Description("Show Details")] public bool ShowDetails { get; set; }
-
     [Description("Owner")] public ApplicationUserDto? Owner { get; set; }
-
-    private class Mapping : Profile
-    {
-        public Mapping()
-        {
-            CreateMap<AuditTrail, AuditTrailDto>(MemberList.None)
-                .ForMember(x => x.PrimaryKey,
-                    s => s.MapFrom(y => JsonSerializer.Serialize(y.PrimaryKey, DefaultJsonSerializerOptions.Options)));
-        }
-    }
+   
 }
