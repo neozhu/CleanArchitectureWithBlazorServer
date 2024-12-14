c3608dd91828bb329e800270f999c409c6ece718
SPYROS\Spyros <sponaris@gmail.com>
2024-12-14 13:05:12 +0200

    .

--- src/Application/Features/OfferLines/Commands/AddEdit/AddEditOfferLineCommand.cs
+++ src/Application/Features/OfferLines/Commands/AddEdit/AddEditOfferLineCommand.cs
@@ -5,12 +5,12 @@ using CleanArchitecture.Blazor.Application.Features.OfferLines.Caching;
 
 namespace CleanArchitecture.Blazor.Application.Features.OfferLines.Commands.AddEdit;
 
-public class AddEditOfferLineCommand: ICacheInvalidatorRequest<Result<int>>
+public class AddEditOfferLineCommand : ICacheInvalidatorRequest<Result<int>>
 {
     [Description("Id")]
-      public int Id { get; set; }
+    public int Id { get; set; }
     [Description("Offer id")]
-    public int OfferId {get;set;} 
+    public int OfferId { get; set; }
 
     private int _itemId;
 
@@ -29,11 +29,12 @@ public class AddEditOfferLineCommand: ICacheInvalidatorRequest<Result<int>>
     }
 
     [Description("Quantity")]
-    public int Quantity {get;set;} 
+    public int Quantity { get; set; } = 1;
+
     [Description("Discount")]
-    public decimal Discount {get;set;} 
+    public decimal Discount { get; set; }
     [Description("Line total")]
-    public decimal LineTotal {get;set;}
+    public decimal LineTotal { get; set; }
     public BehaviorSubject<int> ItemIdBehaviorSubject { get; set; } = new(0);
 
     public string CacheKey => OfferLineCacheKey.GetAllCacheKey;
@@ -57,25 +58,25 @@ public class AddEditOfferLineCommandHandler : IRequestHandler<AddEditOfferLineCo
     {
         if (request.Id > 0)
         {
-   //         var item = await _context.OfferLines.FindAsync(request.Id, cancellationToken);
-   //         if (item == null)
-   //         {
-   //             return await Result<int>.FailureAsync($"OfferLine with id: [{request.Id}] not found.");
-   //         }
-   //         OfferLineMapper.ApplyChangesFrom(request,item);
-			//// raise a update domain event
-			//item.AddDomainEvent(new OfferLineUpdatedEvent(item));
-   //         await _context.SaveChangesAsync(cancellationToken);
-   //         return await Result<int>.SuccessAsync(item.Id);
+            //         var item = await _context.OfferLines.FindAsync(request.Id, cancellationToken);
+            //         if (item == null)
+            //         {
+            //             return await Result<int>.FailureAsync($"OfferLine with id: [{request.Id}] not found.");
+            //         }
+            //         OfferLineMapper.ApplyChangesFrom(request,item);
+            //// raise a update domain event
+            //item.AddDomainEvent(new OfferLineUpdatedEvent(item));
+            //         await _context.SaveChangesAsync(cancellationToken);
+            //         return await Result<int>.SuccessAsync(item.Id);
         }
         else
         {
-   //         var item = OfferLineMapper.FromEditCommand(request);
-   //         // raise a create domain event
-			//item.AddDomainEvent(new OfferLineCreatedEvent(item));
-   //         _context.OfferLines.Add(item);
-   //         await _context.SaveChangesAsync(cancellationToken);
-   //         return await Result<int>.SuccessAsync(item.Id);
+            //         var item = OfferLineMapper.FromEditCommand(request);
+            //         // raise a create domain event
+            //item.AddDomainEvent(new OfferLineCreatedEvent(item));
+            //         _context.OfferLines.Add(item);
+            //         await _context.SaveChangesAsync(cancellationToken);
+            //         return await Result<int>.SuccessAsync(item.Id);
         }
 
         return await Result<int>.SuccessAsync(1);
