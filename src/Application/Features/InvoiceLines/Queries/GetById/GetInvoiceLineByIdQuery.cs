b39b3c0dd21f99887a97347606238504d55527d5
SPYROS\Spyros <sponaris@gmail.com>
2024-12-23 22:48:26 +0200

    .

--- src/Application/Features/InvoiceLines/Queries/GetById/GetInvoiceLineByIdQuery.cs
+++ src/Application/Features/InvoiceLines/Queries/GetById/GetInvoiceLineByIdQuery.cs
@@ -1,36 +1,33 @@
 ﻿using CleanArchitecture.Blazor.Application.Features.InvoiceLines.DTOs;
 using CleanArchitecture.Blazor.Application.Features.InvoiceLines.Caching;
 using CleanArchitecture.Blazor.Application.Features.InvoiceLines.Mappers;
-using CleanArchitecture.Blazor.Application.Features.InvoiceLines.Specifications;
-using System.Diagnostics;
 
 namespace CleanArchitecture.Blazor.Application.Features.InvoiceLines.Queries.GetById;
 
 public class GetInvoiceLineByIdQuery : ICacheableRequest<Result<InvoiceLineDto>>
 {
    public required int Id { get; set; }
+   public required int InvoiceId { get; set; }
    public string CacheKey => InvoiceLineCacheKey.GetByIdCacheKey($"{Id}");
    public IEnumerable<string>? Tags => InvoiceLineCacheKey.Tags;
 }
 
-public class GetInvoiceLineByIdQueryHandler :
+public class GetInvoiceLineByIdQueryHandler(
+    IApplicationDbContext context) :
      IRequestHandler<GetInvoiceLineByIdQuery, Result<InvoiceLineDto>>
 {
-    private readonly IApplicationDbContext _context;
-
-    public GetInvoiceLineByIdQueryHandler(
-        IApplicationDbContext context)
-    {
-        _context = context;
-    }
+    private readonly IApplicationDbContext _context = context;
 
     public async Task<Result<InvoiceLineDto>> Handle(GetInvoiceLineByIdQuery request, CancellationToken cancellationToken)
     {
-        //var data = await _context.InvoiceLines.ApplySpecification(new InvoiceLineByIdSpecification(request.Id))
-        //                                        .ProjectTo()
-        //                                        .FirstAsync(cancellationToken);
-        //return await Result<InvoiceLineDto>.SuccessAsync(data);
+        var data = await _context.Invoices
+              .Where(o => o.Id == request.InvoiceId)
+              .SelectMany(o => o.InvoiceLines)
+              .Where(ol => ol.Id == request.Id)
+              .AsNoTracking()
+              .ProjectTo()
+              .FirstOrDefaultAsync(cancellationToken);
 
-        return null;
+        return await Result<InvoiceLineDto>.SuccessAsync(data);
     }
 }
