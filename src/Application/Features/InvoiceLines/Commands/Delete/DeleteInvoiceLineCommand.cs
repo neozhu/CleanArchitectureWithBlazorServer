6b2ccbee4af26539b963e1112992127a44fcd84d
SPYROS\Spyros <sponaris@gmail.com>
2024-12-24 12:55:07 +0200

    .

--- src/Application/Features/InvoiceLines/Commands/Delete/DeleteInvoiceLineCommand.cs
+++ src/Application/Features/InvoiceLines/Commands/Delete/DeleteInvoiceLineCommand.cs
@@ -8,11 +8,13 @@ namespace CleanArchitecture.Blazor.Application.Features.InvoiceLines.Commands.De
 public class DeleteInvoiceLineCommand:  ICacheInvalidatorRequest<Result<int>>
 {
   public int[] Id {  get; }
+  public int InvoiceId { get; }
   public string CacheKey => InvoiceLineCacheKey.GetAllCacheKey;
   public IEnumerable<string>? Tags => InvoiceLineCacheKey.Tags;
-  public DeleteInvoiceLineCommand(int[] id)
+  public DeleteInvoiceLineCommand(int[] id , int invoiceId)
   {
     Id = id;
+    InvoiceId = invoiceId;
   }
 }
 
@@ -37,7 +39,48 @@ public class DeleteInvoiceLineCommandHandler :
         //     }
         //     var result = await _context.SaveChangesAsync(cancellationToken);
         //     return await Result<int>.SuccessAsync(result);
+
+        var invoice = await _context.Invoices
+            .Include(o => o.InvoiceLines)
+            .FirstOrDefaultAsync(x=> x.Id ==  request.InvoiceId, cancellationToken);
+
+        if (invoice is null)
+            return await Result<int>.FailureAsync($"Invoice with with id: [{request.InvoiceId}] not found.");
+
+        var linesToDelete = invoice.InvoiceLines
+            .Where(x => request.Id.Contains(x.Id))
+            .ToList();
+
+        if (!linesToDelete.Any())
+            return await Result<int>.FailureAsync("No matching OfferLines found to delete.");
+
+        linesToDelete.ForEach(line => invoice.InvoiceLines.Remove(line));
+
+        var result = await _context.SaveChangesAsync(cancellationToken);
+
         return await Result<int>.SuccessAsync(0);
+
+
+        //var offer = await _context.Offers
+        // .Include(o => o.OfferLines)
+        // .FirstOrDefaultAsync(o => o.Id == request.OfferId, cancellationToken);
+
+        //if (offer is null)
+        //    return await Result<int>.FailureAsync($"Offer with id: [{request.OfferId}] not found.");
+
+        //var linesToDelete = offer.OfferLines
+        //    .Where(x => request.Id.Contains(x.Id))
+        //    .ToList();
+
+        //if (!linesToDelete.Any())
+        //    return await Result<int>.FailureAsync("No matching OfferLines found to delete.");
+
+        //linesToDelete.ForEach(line => offer.OfferLines.Remove(line));
+
+        //var result = await _context.SaveChangesAsync(cancellationToken);
+
+        //return await Result<int>.SuccessAsync(result);
+
     }
 
 }
